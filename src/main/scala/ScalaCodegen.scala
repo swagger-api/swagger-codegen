f59e7a73029475e032b34c2aee62c026daeac183
Tony Tam <tony@eatbacon.org>
2012-05-03 12:06:23 -0700

    added runnable object

--- src/main/scala/ScalaCodegen.scala
+++ src/main/scala/ScalaCodegen.scala
@@ -2,6 +2,10 @@ import com.wordnik.swagger.codegen.BasicGenerator
 
 import com.wordnik.swagger.core._
 
+object ScalaCodegen extends ScalaCodegen {
+  def main(args: Array[String]) = generateClient(args)
+}
+
 class ScalaCodegen extends BasicGenerator {
   // set imports for common datatypes
   override def imports = Map(
