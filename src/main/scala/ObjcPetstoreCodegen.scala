1d5d6770a98e3c3b49a82c90e73cd2978f65cb78
Tony Tam <tony@eatbacon.org>
2012-05-08 06:10:01 -0700

    added petstore for objc

---
+++ src/main/scala/ObjcPetstoreCodegen.scala
@@ -0,0 +1,14 @@
+import com.wordnik.swagger.codegen.BasicGenerator
+
+object ObjcPetstoreCodegen extends ObjcCodegen {
+  def main(args: Array[String]) = generateClient(args)
+
+  // where to write generated code
+  override def destinationDir = "samples/objc/"
+
+  // supporting classes
+  override def supportingFiles =
+    List(
+      ("JSONKit.h", destinationDir, "JSONKit.h"),
+      ("JSONKit.m", destinationDir, "JSONKit.m"))
+}
