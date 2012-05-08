72904d9f5f34cd01e55e4f16ced5df79d87482bb
Tony Tam <tony@eatbacon.org>
2012-05-08 14:45:01 -0700

    added objc files

---
+++ src/main/resources/objc/Date.m
@@ -0,0 +1,13 @@
+#import "Date.h"
+
+@implementation Date
+
+@synthesize date = _date;
+
+- (id) initWithValues:(NSNumber *)input {
+    NSTimeInterval interval = [input doubleValue];
+    _date = [[NSDate alloc] initWithTimeIntervalSince1970:interval];
+    return self;
+}
+
+@end
