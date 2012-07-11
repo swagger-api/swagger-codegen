bb0bf0f63734b00ca5bcedd4c93f08b35d7e5025
Kevin Hester <kevinh@geeksville.com>
2012-07-11 11:18:12 -0700

    Don't assume current directory/don't write temp files

--- bin/runscala.sh
+++ bin/runscala.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-echo "" > classpath.txt
-for file in `ls target/*.jar`;
-        do echo -n '' >> classpath.txt;
-        echo -n $file >> classpath.txt;
-        echo -n ':' >> classpath.txt;
-done
-for file in `ls target/lib`;
-	do echo -n 'target/lib/' >> classpath.txt;
-	echo -n $file >> classpath.txt;
-	echo -n ':' >> classpath.txt;
-done
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORDNIK_OPTS=${WORDNIK_OPTS:=NOPE}
 
 if [ $WORDNIK_OPTS = NOPE ];
@@ -19,6 +9,6 @@ if [ $WORDNIK_OPTS = NOPE ];
 	
 fi
 
-export CLASSPATH=$(cat classpath.txt)
-export JAVA_OPTS="${JAVA_OPTS} -DrulePath=data -Xmx4096M -DloggerPath=conf/log4j.properties"
+export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
+export JAVA_OPTS="${JAVA_OPTS} -DrulePath=data -Xmx4096M"
 scala $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS -cp $CLASSPATH "$@"
