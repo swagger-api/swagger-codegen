087798965cb2969f5d5580d424ce8d0fe0836e27
John Kelvie <jpk@alumni.duke.edu>
2013-03-14 10:08:48 -0400

    Removed all Xapp specific stuff from project

--- bin/objc-xapp-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export SCALA_HOME=/Users/jpk/dev/scala-2.9.1.final
-export PATH=$SCALA_HOME/bin:$PATH
-
-export CLASSPATH="$DIR/../target/*:$DIR/../target/lib/*"
-export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-JAVA_OPTS=$JAVA_OPTS scala -cp $CLASSPATH "$@" samples/client/xapp-server/objc/ObjcXappServerCodegen.scala http://localhost:9000/api-docs.json special-key
