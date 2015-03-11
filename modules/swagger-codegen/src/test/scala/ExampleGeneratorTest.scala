bf32514111c8f5317ab3b9e669a196a3f12bcd4f
Tony Tam <fehguy@gmail.com>
2015-03-11 08:36:00 -0700

    updated validation

--- modules/swagger-codegen/src/test/scala/ExampleGeneratorTest.scala
+++ modules/swagger-codegen/src/test/scala/ExampleGeneratorTest.scala
@@ -5,15 +5,12 @@ import com.wordnik.swagger.codegen.examples._
 import com.wordnik.swagger.models.properties._
 import io.swagger.parser._
 
-import javax.xml.bind.annotation._
-import javax.xml.bind.JAXBContext
-import javax.xml.bind.JAXBException
-import javax.xml.bind.Marshaller
-
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
+
+import javax.xml.bind.annotation._
 
 import scala.collection.JavaConverters._
 
@@ -36,68 +33,50 @@ class ExampleGeneratorTest extends FlatSpec with Matchers {
       .property("phones", new ArrayProperty().items(new RefProperty("Phone")))
 
     val user = new ModelImpl()
-      .name("user")
+      .name("User")
       .property("name", new StringProperty())
       .property("id", id)
       .property("address", new RefProperty("Address"))
+      .xml(new Xml().name("user"))
 
     val xml = new XmlExampleGenerator(
       Map[String, Model](
         "User" -> user,
         "Address" -> address,
         "Phone" -> phone).asJava)
-    println(xml.toXml(new RefProperty("User")))
+    val xmlString = xml.toXml(new RefProperty("User"))
+
+    xmlString should be(
+"""<user id="135">
+  <name>string</name>
+  <address>
+    <street>12345 El Monte Blvd</street>
+    <city>Los Altos Hills</city>
+    <state>CA</state>
+    <zip>94022</zip>
+    <phone>
+      <number>(650) 949-7777</number>
+    </phone>
+  </address>
+</user>""")
   }
 }
 
-@RunWith(classOf[JUnitRunner])
-class PojoGenerator extends FlatSpec with Matchers {
-  it should "write xml" in {
-    val phone = new Phone()
-    phone.number = "(650) 949-7777"
-
-    val address = new Address()
-    address.street = "12345 El Monte Blvd"
-    address.phone = phone
-
-    val user = new User();
-    user.name = "dog"
-    user.id = 123L
-    user.address = address
-
-    val jaxbContext = JAXBContext.newInstance( classOf[User] )
-    val jaxbMarshaller = jaxbContext.createMarshaller()
-    jaxbMarshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, true )
-    jaxbMarshaller.marshal( user, System.out )
-  }
-}
-
-@XmlRootElement
 class User {
-  @XmlElement
   var name: String = _
   @XmlAttribute
   var id: Long = _
-  @XmlElement
   var address: Address = _
 }
 
-@XmlRootElement
 class Address {
-  @XmlElement
   var street: String = _
-  @XmlElement
   var city: String = _
-  @XmlElement
   var state: String = _
-  @XmlElement
   var zip: String = _
-  @XmlElement
   var phone: Phone = _
 }
 
-@XmlRootElement
 class Phone {
-  @XmlElement
   var number: String = _
 }
