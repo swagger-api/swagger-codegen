7506c34b77c4fa48d9e73cada91848986adcb676
MerkushevKirill <lanwen@yandex-team.ru>
2015-03-25 12:58:22 +0300

    update docs and bash about CLI usage, deprecate old cli classes
    (after #547)

--- modules/swagger-codegen-distribution/src/main/java/com/wordnik/swagger/codegen/SwaggerCodegen.java
+++ modules/swagger-codegen-distribution/src/main/java/com/wordnik/swagger/codegen/SwaggerCodegen.java
@@ -10,6 +10,10 @@ import io.airlift.airline.Help;
  * User: lanwen
  * Date: 24.03.15
  * Time: 17:56
+ *
+ * Command line interface for swagger codegen
+ *
+ * @since 2.1.3-M1
  */
 public class SwaggerCodegen {
 
