c5f95dd139bf99a9d48e1e2100b13b307840b0a2
wing328 <wing328hk@gmail.com>
2015-04-10 08:41:10 +0800

    rebase develop_2.0 (resolve conflicts)

--- samples/client/petstore/php/test.php
+++ samples/client/petstore/php/test.php
@@ -4,7 +4,7 @@ require_once('SwaggerPetstore-php/SwaggerPetstore.php');
 
 // initialize the API client
 $api_client = new SwaggerPetstore\APIClient('http://petstore.swagger.io/v2');
-$petId = 5;  // ID of pet that needs to be fetched
+$petId = 10002;  // ID of pet that needs to be fetched
 try {
     $pet_api = new SwaggerPetstore\PetAPI($api_client);
     // return Pet (model)
