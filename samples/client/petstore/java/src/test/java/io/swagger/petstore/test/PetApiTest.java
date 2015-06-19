7565f22da7ba0d9c11b13a980ac2f981b35debe9
xhh <xhh@xhh.me>
2015-06-19 11:24:39 +0800

    Add test cases for async API call
    Also add teardown method to delete the created random pet after each test

--- samples/client/petstore/java/src/test/java/io/swagger/petstore/test/PetApiTest.java
+++ samples/client/petstore/java/src/test/java/io/swagger/petstore/test/PetApiTest.java
@@ -1,8 +1,6 @@
 package io.swagger.petstore.test;
 
-import io.swagger.client.ApiClient;
-import io.swagger.client.ApiException;
-import io.swagger.client.Configuration;
+import io.swagger.client.*;
 
 import io.swagger.client.api.*;
 import io.swagger.client.auth.*;
@@ -14,19 +12,30 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PetApiTest {
     PetApi api = null;
+    Pet pet = null;
 
     @Before
-    public void setup() {
+    public void setup() throws ApiException {
         api = new PetApi();
         // setup authentication
         ApiKeyAuth apiKeyAuth = (ApiKeyAuth) api.getApiClient().getAuthentication("api_key");
         apiKeyAuth.setApiKey("special-key");
+
+        pet = createRandomPet();
+        api.addPet(pet);
+    }
+
+    @After
+    public void teardown() throws ApiException {
+        api.deletePet(null, pet.getId());
     }
 
     @Test
@@ -58,9 +67,6 @@ public class PetApiTest {
 
     @Test
     public void testCreateAndGetPet() throws Exception {
-        Pet pet = createRandomPet();
-        api.addPet(pet);
-
         Pet fetched = api.getPetById(pet.getId());
         assertNotNull(fetched);
         assertEquals(pet.getId(), fetched.getId());
@@ -69,8 +75,79 @@ public class PetApiTest {
     }
 
     @Test
+    public void testCreateAndGetPetAsync() throws Exception {
+        // to store returned Pet or error message/exception
+        final Map<String, Object> result = new HashMap<String, Object>();
+        api.getPetByIdAsync(pet.getId(), new ApiCallback<Pet>() {
+            @Override
+            public void onFailure(ApiException e) {
+                result.put("error", e.getMessage());
+            }
+
+            @Override
+            public void onSuccess(Pet pet) {
+                result.put("pet", pet);
+            }
+        });
+        // the API call should be executed asynchronously, so result should be empty at the moment
+        assertTrue(result.isEmpty());
+
+        // wait for the asynchronous call to finish (at most 10 seconds)
+        final int maxTry = 10;
+        int tryCount = 1;
+        Pet fetched = null;
+        do {
+            if (tryCount > maxTry) fail("have not got result of getPetByIdAsync after 10 seconds");
+            Thread.sleep(1000);
+            tryCount += 1;
+            if (result.get("error") != null) fail((String) result.get("error"));
+            if (result.get("pet") != null) {
+                fetched = (Pet) result.get("pet");
+                break;
+            }
+        } while (result.isEmpty());
+        assertNotNull(fetched);
+        assertEquals(pet.getId(), fetched.getId());
+        assertNotNull(fetched.getCategory());
+        assertEquals(fetched.getCategory().getName(), pet.getCategory().getName());
+
+        // test getting a nonexistent pet
+        result.clear();
+        api.getPetByIdAsync(new Long(-1), new ApiCallback<Pet>() {
+            @Override
+            public void onFailure(ApiException e) {
+                result.put("exception", e);
+            }
+
+            @Override
+            public void onSuccess(Pet pet) {
+                result.put("pet", pet);
+            }
+        });
+        // the API call should be executed asynchronously, so result should be empty at the moment
+        assertTrue(result.isEmpty());
+
+        // wait for the asynchronous call to finish (at most 10 seconds)
+        tryCount = 1;
+        ApiException exception = null;
+        do {
+            if (tryCount > maxTry) fail("have not got result of getPetByIdAsync after 10 seconds");
+            Thread.sleep(1000);
+            tryCount += 1;
+            if (result.get("pet") != null) fail("expected an error");
+            if (result.get("exception") != null) {
+                exception = (ApiException) result.get("exception");
+                break;
+            }
+        } while (result.isEmpty());
+        assertNotNull(exception);
+        assertEquals(404, exception.getCode());
+        assertEquals("Not Found", exception.getMessage());
+        assertEquals("application/json", exception.getResponseHeaders().get("Content-Type").get(0));
+    }
+
+    @Test
     public void testUpdatePet() throws Exception {
-        Pet pet = createRandomPet();
         pet.setName("programmer");
 
         api.updatePet(pet);
@@ -84,7 +161,6 @@ public class PetApiTest {
 
     @Test
     public void testFindPetsByStatus() throws Exception {
-        Pet pet = createRandomPet();
         pet.setName("programmer");
         pet.setStatus(Pet.StatusEnum.available);
 
@@ -106,7 +182,6 @@ public class PetApiTest {
 
     @Test
     public void testFindPetsByTags() throws Exception {
-        Pet pet = createRandomPet();
         pet.setName("monster");
         pet.setStatus(Pet.StatusEnum.available);
 
@@ -133,7 +208,6 @@ public class PetApiTest {
 
     @Test
     public void testUpdatePetWithForm() throws Exception {
-        Pet pet = createRandomPet();
         pet.setName("frank");
         api.addPet(pet);
 
@@ -147,9 +221,6 @@ public class PetApiTest {
 
     @Test
     public void testDeletePet() throws Exception {
-        Pet pet = createRandomPet();
-        api.addPet(pet);
-
         Pet fetched = api.getPetById(pet.getId());
         api.deletePet(null, fetched.getId());
 
@@ -163,9 +234,6 @@ public class PetApiTest {
 
     @Test
     public void testUploadFile() throws Exception {
-        Pet pet = createRandomPet();
-        api.addPet(pet);
-
         File file = new File("hello.txt");
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         writer.write("Hello world!");
