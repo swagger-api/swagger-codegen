6b251e7e82149063aacdb008c3ad58db39502a04
Ron <webron@gmail.com>
2015-08-24 11:17:56 +0300

    Redirecting development to the master branch

--- samples/client/petstore/typescript-angular/API/Client/api.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="User.ts" />
-/// <reference path="Category.ts" />
-/// <reference path="Pet.ts" />
-/// <reference path="Tag.ts" />
-/// <reference path="Order.ts" />
-
-/// <reference path="UserApi.ts" />
-/// <reference path="PetApi.ts" />
-/// <reference path="StoreApi.ts" />
