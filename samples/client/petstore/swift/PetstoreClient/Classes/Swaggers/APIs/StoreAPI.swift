8d856d636b6fe161f7467b2f1b2c1ce26419cca0
Joseph Zuromski <jaz@activehours.com>
2016-02-09 09:15:54 -0800

    update sample client code

--- samples/client/petstore/swift/PetstoreClient/Classes/Swaggers/APIs/StoreAPI.swift
+++ samples/client/petstore/swift/PetstoreClient/Classes/Swaggers/APIs/StoreAPI.swift
@@ -52,14 +52,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2016-02-07T13:55:23.709+0000",
+  "shipDate" : "2016-02-09T17:14:23.828+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2016-02-07T05:55:23.712Z</shipDate>
+  <shipDate>2016-02-09T09:14:23.831Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
@@ -67,14 +67,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2016-02-07T13:55:23.709+0000",
+  "shipDate" : "2016-02-09T17:14:23.828+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2016-02-07T05:55:23.712Z</shipDate>
+  <shipDate>2016-02-09T09:14:23.831Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
@@ -104,14 +104,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2016-02-07T13:55:23.713+0000",
+  "shipDate" : "2016-02-09T17:14:23.832+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2016-02-07T05:55:23.713Z</shipDate>
+  <shipDate>2016-02-09T09:14:23.832Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
@@ -119,14 +119,14 @@ extension PetstoreClientAPI {
   "petId" : 123456789,
   "quantity" : 123,
   "id" : 123456789,
-  "shipDate" : "2016-02-07T13:55:23.713+0000",
+  "shipDate" : "2016-02-09T17:14:23.832+0000",
   "complete" : true,
   "status" : "aeiou"
 }}, {contentType=application/xml, example=<Order>
   <id>123456</id>
   <petId>123456</petId>
   <quantity>0</quantity>
-  <shipDate>2016-02-07T05:55:23.713Z</shipDate>
+  <shipDate>2016-02-09T09:14:23.832Z</shipDate>
   <status>string</status>
   <complete>true</complete>
 </Order>}]
