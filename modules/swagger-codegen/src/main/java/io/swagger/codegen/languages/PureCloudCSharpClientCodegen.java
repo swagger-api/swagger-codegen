e89c3d58fd76280ab7b557a47056c890861741af
Smith, Tim <Tim.Smith@inin.com>
2016-02-17 14:19:25 -0700

    purecloud-specific overrides

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PureCloudCSharpClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PureCloudCSharpClientCodegen.java
@@ -1,31 +1,44 @@
 package io.swagger.codegen.languages;
 
+import io.swagger.models.Operation;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PureCloudCSharpClientCodegen extends CSharpClientCodegen {
+
+    protected Logger LOGGER = LoggerFactory.getLogger(PureCloudCSharpClientCodegen.class);
 
     public PureCloudCSharpClientCodegen() {
         super();
 
-        // Override some variables
-        packageTitle = "PureCloud Public API Library";
-        packageProductName = "PureCloudPublicAPILibrary";
-        packageDescription = "A .NET library to interface with the PureCloud Public API";
-        packageCompany = "Interactive Intelligence, Inc.";
-        packageCopyright = "Copyright © Interactive Intelligence, Inc. 2015";
-        packageName = "ININ.PureCloud.PublicAPI";
-        apiPackage = packageName + ".Api";
-        modelPackage = packageName + ".Model";
-        clientPackage = packageName + ".Client";
-        //TODO: determine this programatically somehow
-        packageVersion = "0.0.0.1";
+        // Use C# templates
+        embeddedTemplateDir = templateDir = "csharp";
 
-        /* There is a type named "PureCloud" that must be fully qualified since the compiler assumes that "PureCloud"
-         * means the namespace "ININ.PureCloud"
-         */
-        typeMapping.put("PureCloud", "ININ.PureCloud.PublicAPI.Model.PureCloud");
+        // Prevents collision between System.Attribute and ININ.PureCloudApi.Model.Attribute
+        typeMapping.put("Attribute", "ININ.PureCloudApi.Model.Attribute");
     }
 
     @Override
     public String getName() {
         return "purecloudcsharp";
+    }
+
+    @Override
+    /**
+     * Get the value of x-inin-method-name, or use default C# behavior if blank.
+     *
+     * @param operation the operation object
+     * @param path the path of the operation
+     * @param httpMethod the HTTP method of the operation
+     * @return the (generated) operationId
+     */
+    protected String getOrGenerateOperationId(Operation operation, String path, String httpMethod) {
+        if (operation.getVendorExtensions().containsKey("x-inin-method-name")) {
+            String ininMethodName = operation.getVendorExtensions().get("x-inin-method-name").toString();
+            if (!StringUtils.isBlank(ininMethodName)) return ininMethodName;
+        }
+
+        return super.getOrGenerateOperationId(operation, path, httpMethod);
     }
 }
