fe190cb260d8d77dec4217980ba75bd15e41ccdc
Smith, Tim <Tim.Smith@inin.com>
2016-03-10 15:53:09 -0700

    enum updates

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/CSharpClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/CSharpClientCodegen.java
@@ -354,8 +354,7 @@ public class CSharpClientCodegen extends AbstractCSharpCodegen {
 
                 // Use the basic type (i.e. List<string>) instead of the enum collection (i.e. List<MyEnum>)
                 if (var.items != null && var.items.datatypeWithEnum != null) {
-                    LOGGER.info("$$$$ Changing datatypeWithEnum '" + var.items.datatypeWithEnum + "' to '" + var.items.datatype + "'");
-                    var.items.datatypeWithEnum = var.items.datatype;
+                    var.datatypeWithEnum = var.datatype;
                 }
             }
         }
