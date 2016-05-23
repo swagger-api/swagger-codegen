0c1b970e2c63a6c9c7fe01968356e69597007a81
Mateusz Mackowiak <mmackowiak@mobiltek.pl>
2016-05-23 22:36:07 +0200

    [Objc] Basic Core data support

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/ObjcClientOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/ObjcClientOptionsProvider.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 public class ObjcClientOptionsProvider implements OptionsProvider {
     public static final String CLASS_PREFIX_VALUE = "SWGObjc";
+    public static final String CORE_DATA_VALUE = "n";
     public static final String POD_NAME_VALUE = "SwaggerClientObjc";
     public static final String POD_VERSION_VALUE = "1.0.0-SNAPSHOT";
     public static final String AUTHOR_NAME_VALUE = "SwaggerObjc";
@@ -31,6 +32,7 @@ public class ObjcClientOptionsProvider implements OptionsProvider {
                 .put(ObjcClientCodegen.AUTHOR_EMAIL, AUTHOR_EMAIL_VALUE)
                 .put(ObjcClientCodegen.GIT_REPO_URL, GIT_REPO_URL_VALUE)
                 .put(ObjcClientCodegen.LICENSE, LICENSE_VALUE)
+                .put(ObjcClientCodegen.CORE_DATA, CORE_DATA_VALUE)
                 .build();
     }
 
