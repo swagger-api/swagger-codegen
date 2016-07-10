0b8cb18d7d9df1ae4bc339515d073a312fdf835e
wing328 <wing328hk@gmail.com>
2016-07-11 02:00:04 +0800

    update windows batch file to shorten the commands

--- bin/windows/nancyfx-petstore-server.bat
+++ bin/windows/nancyfx-petstore-server.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate -t modules\swagger-codegen\src\main\resources\nancyfx -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l nancyfx -o samples\server\petstore\nancyfx\
+set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l nancyfx -o samples\server\petstore\nancyfx\
 
 java %JAVA_OPTS% -jar %executable% %ags%
