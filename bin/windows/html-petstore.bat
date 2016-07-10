860c31d5df3e62c2ac9e9799e274f13f8474b15e
wing328 <wing328hk@gmail.com>
2016-07-10 23:53:28 +0800

    fix html batch script

--- bin/windows/html-petstore.bat
+++ bin/windows/html-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate -t modules\swagger-codegen\src\main\resources\html -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l html -o samples\client\petstore\html
+set ags=generate -t modules\swagger-codegen\src\main\resources\htmlDocs -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l html -o samples\client\petstore\html
 
 java %JAVA_OPTS% -jar %executable% %ags%
