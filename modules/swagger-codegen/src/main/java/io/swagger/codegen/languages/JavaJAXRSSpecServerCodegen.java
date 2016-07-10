73dcbdf482f2ae3487fcc366f820de29fe7b7255
wing328 <wing328hk@gmail.com>
2016-07-11 00:20:22 +0800

    fix missing api, model test in jaxrs spec generator

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/JavaJAXRSSpecServerCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/JavaJAXRSSpecServerCodegen.java
@@ -36,6 +36,9 @@ public class JavaJAXRSSpecServerCodegen extends AbstractJavaJAXRSServerCodegen
         apiPackage = "io.swagger.api";
         modelPackage = "io.swagger.model";
 
+        apiTestTemplateFiles.clear(); // TODO: add api test template
+        modelTestTemplateFiles.clear(); // TODO: add model test template
+
         // clear model and api doc template as this codegen
         // does not support auto-generated markdown doc at the moment
         //TODO: add doc templates
