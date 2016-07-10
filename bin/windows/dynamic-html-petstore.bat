c07c5937b7fafe4bae612a6a2f132267d3af410e
wing328 <wing328hk@gmail.com>
2016-07-10 22:08:16 +0800

    show log after test script

--- bin/windows/dynamic-html-petstore.bat
+++ bin/windows/dynamic-html-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate -t modules\swagger-codegen\src\main\resources\dynamic-html -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l dynamic-html -o samples\client\petstore\dynamic-html
+set ags=generate -t modules\swagger-codegen\src\main\resources\swagger-static -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l dynamic-html -o samples\client\petstore\dynamic-html
 
 java %JAVA_OPTS% -jar %executable% %ags%
