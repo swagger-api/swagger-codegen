8127bed1f21865b21e6ef6f78ddcf16b2fd5fe5a
tao <tao@wealthfront.com>
2016-07-18 17:35:40 -0700

    stop shortening underscore

---
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/languages/AbstractJavaCodegenTest.java
@@ -0,0 +1,16 @@
+package io.swagger.codegen.languages;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AbstractJavaCodegenTest {
+
+  @Test
+  public void toEnumVarNameShouldNotShortenUnderScore() throws Exception {
+    AbstractJavaCodegen javaCodegen = new FakeJavaCodegen();
+    Assert.assertEquals("_", javaCodegen.toEnumVarName("_", "String"));
+    Assert.assertEquals("__", javaCodegen.toEnumVarName("__", "String"));
+    Assert.assertEquals("__", javaCodegen.toEnumVarName("_,.", "String"));
+  }
+
+}
---
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/languages/FakeJavaCodegen.java
@@ -0,0 +1,22 @@
+package io.swagger.codegen.languages;
+
+import io.swagger.codegen.CodegenType;
+
+public class FakeJavaCodegen extends AbstractJavaCodegen {
+
+  @Override
+  public CodegenType getTag() {
+    return null;
+  }
+
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public String getHelp() {
+    return null;
+  }
+
+}
