739c3327a61f339747ce50686211d01c267e8390
Smith, Tim <Tim.Smith@inin.com>
2016-08-05 08:23:28 -0600

    NO-JIRA Updating C# script for local build

--- bin/purecloud-csharp.sh
+++ bin/purecloud-csharp.sh
@@ -24,8 +24,14 @@ then
   mvn clean package
 fi
 
+rm -rf dists/purecloud/csharp
+mkdir -p dists/purecloud/csharp/src/ININ.PureCloudApi/Extensions
+
+
+cp -R /git/PureCloudApiLibrary_CSharp/Extensions dists/purecloud/csharp/src/ININ.PureCloudApi/Extensions
+
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -i https://api.mypurecloud.com/api/v2/docs/swagger -l purecloudcsharp -o dists/purecloud/csharp -c bin/config/purecloud-csharp.json"
+ags="$@ generate -i https://api.mypurecloud.com/api/v2/docs/swagger -l purecloudcsharp -o dists/purecloud/csharp -c bin/config/purecloud-csharp.json -t /git/PureCloudApiLibrary_CSharp/swagger_template"
 
 java $JAVA_OPTS -jar $executable $ags
