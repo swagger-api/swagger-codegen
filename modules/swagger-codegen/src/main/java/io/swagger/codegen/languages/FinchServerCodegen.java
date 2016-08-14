fc822fb1e6874b630a1614758b1714c5328b94c0
wing328 <wing328hk@gmail.com>
2016-08-14 16:07:15 +0800

    add finch generator and resource

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/FinchServerCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/FinchServerCodegen.java
@@ -4,27 +4,34 @@ import io.swagger.codegen.CliOption;
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.CodegenConstants;
 import io.swagger.codegen.CodegenOperation;
+import io.swagger.codegen.CodegenParameter;
 import io.swagger.codegen.CodegenType;
+import io.swagger.codegen.DefaultCodegen;
 import io.swagger.codegen.SupportingFile;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.MapProperty;
+import io.swagger.models.properties.Property;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-public class ScalatraServerCodegen extends AbstractScalaCodegen implements CodegenConfig {
-
+public class FinchServerCodegen extends DefaultCodegen implements CodegenConfig {
+    protected String invokerPackage = "io.swagger.client";
     protected String groupId = "io.swagger";
     protected String artifactId = "swagger-client";
     protected String artifactVersion = "1.0.0";
+    protected String sourceFolder = "src/main/scala";
 
-    public ScalatraServerCodegen() {
+    public FinchServerCodegen() {
         super();
-        outputFolder = "generated-code/scalatra";
+        outputFolder = "generated-code/finch";
         modelTemplateFiles.put("model.mustache", ".scala");
         apiTemplateFiles.put("api.mustache", ".scala");
-        embeddedTemplateDir = templateDir = "scalatra";
+        embeddedTemplateDir = templateDir = "finch";
         apiPackage = "com.wordnik.client.api";
         modelPackage = "com.wordnik.client.model";
 
@@ -80,12 +87,25 @@ public class ScalatraServerCodegen extends AbstractScalaCodegen implements Codeg
         supportingFiles.add(new SupportingFile("build.sbt", "", "build.sbt"));
         supportingFiles.add(new SupportingFile("web.xml", "/src/main/webapp/WEB-INF", "web.xml"));
         supportingFiles.add(new SupportingFile("JettyMain.mustache", sourceFolder, "JettyMain.scala"));
-        supportingFiles.add(new SupportingFile("Bootstrap.mustache", sourceFolder, "ScalatraBootstrap.scala"));
+        supportingFiles.add(new SupportingFile("Bootstrap.mustache", sourceFolder, "FinchBootstrap.scala"));
         supportingFiles.add(new SupportingFile("ServletApp.mustache", sourceFolder, "ServletApp.scala"));
         supportingFiles.add(new SupportingFile("project/build.properties", "project", "build.properties"));
         supportingFiles.add(new SupportingFile("project/plugins.sbt", "project", "plugins.sbt"));
         supportingFiles.add(new SupportingFile("sbt", "", "sbt"));
 
+        supportingFiles.add(new SupportingFile("endpoint.mustache", sourceFolder, "endpoint.scala"));
+
+        languageSpecificPrimitives = new HashSet<String>(
+                Arrays.asList(
+                        "String",
+                        "boolean",
+                        "Boolean",
+                        "Double",
+                        "Integer",
+                        "Long",
+                        "Float",
+                        "Object")
+        );
         instantiationTypes.put("array", "ArrayList");
         instantiationTypes.put("map", "HashMap");
 
@@ -103,6 +123,9 @@ public class ScalatraServerCodegen extends AbstractScalaCodegen implements Codeg
         importMapping.put("LocalDateTime", "org.joda.time.LocalDateTime");
         importMapping.put("LocalDate", "org.joda.time.LocalDate");
         importMapping.put("LocalTime", "org.joda.time.LocalTime");
+
+        cliOptions.add(new CliOption(CodegenConstants.MODEL_PACKAGE, CodegenConstants.MODEL_PACKAGE_DESC));
+        cliOptions.add(new CliOption(CodegenConstants.API_PACKAGE, CodegenConstants.API_PACKAGE_DESC));
     }
 
     @Override
@@ -112,12 +135,27 @@ public class ScalatraServerCodegen extends AbstractScalaCodegen implements Codeg
 
     @Override
     public String getName() {
-        return "scalatra";
+        return "finch";
     }
 
     @Override
     public String getHelp() {
-        return "Generates a Scala server application with Scalatra.";
+        return "Generates a Scala server application with Finch.";
+    }
+
+    @Override
+    public String escapeReservedWord(String name) {
+        return "_" + name;
+    }
+
+    @Override
+    public String apiFileFolder() {
+        return outputFolder + "/" + sourceFolder + "/" + apiPackage().replace('.', File.separatorChar);
+    }
+
+    @Override
+    public String modelFileFolder() {
+        return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
     }
 
     @Override
@@ -125,37 +163,91 @@ public class ScalatraServerCodegen extends AbstractScalaCodegen implements Codeg
         Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
         List<CodegenOperation> operationList = (List<CodegenOperation>) operations.get("operation");
         for (CodegenOperation op : operationList) {
-            // force http method to lower case
             op.httpMethod = op.httpMethod.toLowerCase();
- 
-            String[] items = op.path.split("/", -1);
+
+            String path = new String(op.path);
+            // remove first /
+            if (path.startsWith("/")) {
+                path = path.substring(1);
+            }
+            // remove last /
+            if (path.endsWith("/")) {
+                path = path.substring(0, path.length()-1);
+            }
+
+            LOGGER.info("processing path: " + path);
+            String[] items = path.split("/", -1);
             String scalaPath = "";
             int pathParamIndex = 0;
 
             for (int i = 0; i < items.length; ++i) {
+                //String i : items) {
+                LOGGER.info("processing item: " + i);
                 if (items[i].matches("^\\{(.*)\\}$")) { // wrap in {}
-                    scalaPath = scalaPath + ":" + items[i].replace("{", "").replace("}", "");
+                    // find the datatype of the parameter
+                    final CodegenParameter cp = op.pathParams.get(pathParamIndex);
+                    scalaPath = scalaPath + cp.dataType;
                     pathParamIndex++;
                 } else {
-                    scalaPath = scalaPath + items[i];
+                    scalaPath = scalaPath + "\"" + items[i] + "\"";
                 }
 
                 if (i != items.length -1) {
-                    scalaPath = scalaPath + "/";
+                    scalaPath = scalaPath + " / ";
                 }
+                LOGGER.info("processing scalaPath: " + scalaPath);
             }
 
-            op.vendorExtensions.put("x-scalatra-path", scalaPath);
-        }
+            // add ? to indicate query/header/form parameter
+            if (op.formParams.size() > 0 || op.queryParams.size() > 0 || op.headerParams.size() > 0) {
+                scalaPath = scalaPath + " ?";
+            }
 
+            op.vendorExtensions.put("x-scala-path", scalaPath);
+        }
         return objs;
     }
 
 
     @Override
+    public String getTypeDeclaration(Property p) {
+        if (p instanceof ArrayProperty) {
+            ArrayProperty ap = (ArrayProperty) p;
+            Property inner = ap.getItems();
+            return getSwaggerType(p) + "[" + getTypeDeclaration(inner) + "]";
+        } else if (p instanceof MapProperty) {
+            MapProperty mp = (MapProperty) p;
+            Property inner = mp.getAdditionalProperties();
+
+            return getSwaggerType(p) + "[String, " + getTypeDeclaration(inner) + "]";
+        }
+        return super.getTypeDeclaration(p);
+    }
+
+    @Override
+    public String getSwaggerType(Property p) {
+        String swaggerType = super.getSwaggerType(p);
+        String type = null;
+        if (typeMapping.containsKey(swaggerType)) {
+            type = typeMapping.get(swaggerType);
+            if (languageSpecificPrimitives.contains(type)) {
+                return toModelName(type);
+            }
+        } else {
+            type = swaggerType;
+        }
+        return toModelName(type);
+    }
+
+    @Override
     public String escapeQuotationMark(String input) {
         // remove " to avoid code injection
         return input.replace("\"", "");
+    }
+
+    @Override
+    public String escapeUnsafeCharacters(String input) {
+        return input.replace("*/", "*_/").replace("/*", "/_*");
     }
 
 }
