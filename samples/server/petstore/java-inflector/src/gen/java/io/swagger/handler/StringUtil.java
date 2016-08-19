7ecf8d0c962ea94e563ad9725971d2945e3d62ff
wing328 <wing328hk@gmail.com>
2016-08-19 20:09:06 +0800

    update java inflector to fix enum tostring

--- samples/server/petstore/java-inflector/src/gen/java/io/swagger/handler/StringUtil.java
+++ samples/server/petstore/java-inflector/src/gen/java/io/swagger/handler/StringUtil.java
@@ -1,6 +1,6 @@
 package io.swagger.handler;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaInflectorServerCodegen", date = "2016-08-17T22:41:52.290+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaInflectorServerCodegen", date = "2016-08-19T20:03:56.681+08:00")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
