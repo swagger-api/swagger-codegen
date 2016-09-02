895d5002711493f2b5f58ac4fd775cd91b8b8b5e
Smith, Tim <Tim.Smith@inin.com>
2016-09-02 14:33:49 -0600

    API-1903 Moving enum handling into codegen project

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PureCloudJavaClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PureCloudJavaClientCodegen.java
@@ -1,13 +1,15 @@
 package io.swagger.codegen.languages;
 
+import io.swagger.codegen.CodegenModel;
+import io.swagger.codegen.CodegenProperty;
 import io.swagger.models.Operation;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by tim.smith on 4/5/16.
- */
+import java.util.*;
+
+
 public class PureCloudJavaClientCodegen extends JavaClientCodegen {
 
     protected Logger LOGGER = LoggerFactory.getLogger(PureCloudJavaClientCodegen.class);
@@ -42,5 +44,52 @@ public class PureCloudJavaClientCodegen extends JavaClientCodegen {
         }
 
         return super.getOrGenerateOperationId(operation, path, httpMethod);
+    }
+
+    @Override
+    public Map<String, Object> postProcessModelsEnum(Map<String, Object> objs) {
+        objs = super.postProcessModelsEnum(objs);
+
+        List<Object> models = (List<Object>) objs.get("models");
+
+        // Iterate through models
+        for (Object _mo : models) {
+            Map<String, Object> mo = (Map<String, Object>) _mo;
+            CodegenModel cm = (CodegenModel) mo.get("model");
+
+            // Iterate through properties
+            for (CodegenProperty cp : cm.allVars) {
+                // Enums with values only
+                if (cp.isEnum && cp.allowableValues != null) {
+                    Object valuesObject = cp.allowableValues.get("values");
+                    if (valuesObject != null) {
+                        ArrayList valuesArray = (ArrayList) valuesObject;
+                        if (valuesArray.get(0) instanceof Integer) {
+                            // Integer enum type
+                            System.out.println("Adding 'OUTDATEDSDKVERSION(-1)' to " + cm.name + "." + cp.name + "Enum");
+                            valuesArray.add(0, -1);
+                            Object enumVarsObject = cp.allowableValues.get("enumVars");
+                            ArrayList enumVarsArray = (ArrayList) enumVarsObject;
+                            HashMap<String, String> newItem = new HashMap<String, String>();
+                            newItem.put("name", "OUTDATEDSDKVERSION");
+                            newItem.put("value", toEnumValue("-1", "Integer"));
+                            enumVarsArray.add(0, newItem);
+                        } else {
+                            // String enum type
+                            System.out.println("Adding 'OUTDATEDSDKVERSION(\"OutdatedSdkVersion\")' to " + cm.name + "." + cp.name + "Enum");
+                            valuesArray.add(0, "OutdatedSdkVersion");
+                            Object enumVarsObject = cp.allowableValues.get("enumVars");
+                            ArrayList enumVarsArray = (ArrayList) enumVarsObject;
+                            HashMap<String, String> newItem = new HashMap<String, String>();
+                            newItem.put("name", "OUTDATEDSDKVERSION");
+                            newItem.put("value", toEnumValue("OutdatedSdkVersion", "String"));
+                            enumVarsArray.add(0, newItem);
+                        }
+                    }
+                }
+            }
+
+        }
+        return objs;
     }
 }
