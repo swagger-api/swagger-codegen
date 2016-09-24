ba98ce1159353d7f437741ce7456419e06fea98f
Steve Hu <stevehu@gmail.com>
2016-09-23 20:47:15 -0400

    add secondary.crt new token with petstore scopes, upgrade to 0.1.2 and security.json new format

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/UndertowCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/UndertowCodegen.java
@@ -76,6 +76,7 @@ public class UndertowCodegen extends AbstractJavaCodegen {
         supportingFiles.add(new SupportingFile("server.json", ("src.main.resources.config").replace(".", java.io.File.separator), "server.json"));
         supportingFiles.add(new SupportingFile("security.json", ("src.main.resources.config").replace(".", java.io.File.separator), "security.json"));
         supportingFiles.add(new SupportingFile("primary.crt", ("src.main.resources.config.oauth").replace(".", java.io.File.separator), "primary.crt"));
+        supportingFiles.add(new SupportingFile("secondary.crt", ("src.main.resources.config.oauth").replace(".", java.io.File.separator), "secondary.crt"));
 
     }
 
