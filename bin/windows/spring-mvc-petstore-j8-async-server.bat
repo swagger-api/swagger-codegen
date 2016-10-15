29e29489bec959e4c090e14d39a618241381350e
jfiala <johannes.fiala@fwd.at>
2016-10-15 20:39:17 +0200

    Revert "re-create bin folder"
    This reverts commit 8a6d41314784a2ae5ce29ae54d7db1ebec418751.
    
    Conflicts:
    	bin/windows/spring-mvc-petstore-j8-async-server.bat
    	bin/windows/spring-mvc-petstore-server.bat
    	bin/windows/springboot-petstore-server.bat

--- bin/windows/spring-mvc-petstore-j8-async-server.bat
+++ bin/windows/spring-mvc-petstore-j8-async-server.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 set executable=.\modules\swagger-codegen-cli\target\swagger-codegen-cli.jar
 
 If Not Exist %executable% (
@@ -9,15 +8,3 @@ REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
 set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l spring --library=spring-mvc -o samples\server\petstore\spring-mvc-j8-async -c bin\spring-mvc-petstore-j8-async.json
 
 java %JAVA_OPTS% -jar %executable% %ags%
-=======
-set executable=.\modules\swagger-codegen-cli\target\swagger-codegen-cli.jar
-
-If Not Exist %executable% (
-  mvn clean package
-)
-
-REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
-set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.json -l spring --library=spring-mvc -o samples\server\petstore\spring-mvc-j8-async -c bin\spring-mvc-petstore-j8-async.json
-
-java %JAVA_OPTS% -jar %executable% %ags%
->>>>>>> parent of 82df5e6... temporary solution for pattern - move to AbstractJavaCodegen #2549
