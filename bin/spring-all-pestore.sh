6815c628806b1814f8353a40ed65ed97312f9b22
jfiala <johannes.fiala@fwd.at>
2016-10-15 20:42:49 +0200

    Revert "re-create bin folder"
    This reverts commit 8a6d41314784a2ae5ce29ae54d7db1ebec418751.

--- bin/spring-all-pestore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./bin/spring-cloud-feign-petstore.sh
-./bin/spring-stubs.sh
-./bin/spring-mvc-petstore-j8-async-server.sh
-./bin/springboot-petstore-server.sh
-./bin/spring-mvc-petstore-server.sh
