33bc6e79a7a250d25f71f6596b20df7e8049cca7
swativerma11 <swati.verma@capillarytech.com>
2016-12-16 16:22:46 +0530

    removed build number

--- swagger_sdk_gen.sh
+++ swagger_sdk_gen.sh
@@ -8,23 +8,23 @@ then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar genera
   -i http://newapi.nightly.capillary.in/swagger.json  \
   -l java \
   -DdateLibrary=java8 \
-  -o intouch_api/java_client/java_$BUILD_NUMBER \
+  -o intouch_api/java_client/java \
   -c config.json
-  tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java_$BUILD_NUMBER/ .
-  mvn3 clean deploy -f intouch_api/java_client/java_$BUILD_NUMBER/pom.xml
+  tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
+  mvn3 clean deploy -f intouch_api/java_client/java/pom.xml
 elif [ "$Client" = "c#" ]
 then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i http://newapi.nightly.capillary.in/swagger.json \
   -l csharp\
   -DtargetFramework=v$Version \
-  -o intouch_api/csharp_client/c#_$BUILD_NUMBER
-  tar cvzf intouch_api/csharp_client/c#swagger_sdK_$BUILD_NUMBER.tar.gz -C ./intouch_api/csharp_client/c#_$BUILD_NUMBER/ .
+  -o intouch_api/csharp_client/c#
+  tar cvzf intouch_api/csharp_client/c#swagger_sdK_$BUILD_NUMBER.tar.gz -C ./intouch_api/csharp_client/c#/ .
 elif [ "$Client" = "php" ]
 then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i http://newapi.nightly.capillary.in/swagger.json  \
   -l php \
-  -o intouch_api/php_client/php_$BUILD_NUMBER
-  tar cvzf intouch_api/php_client/php_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/php_client/php_$BUILD_NUMBER/ .
+  -o intouch_api/php_client/php
+  tar cvzf intouch_api/php_client/php_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/php_client/php/ .
 elif [ "$Client" = "nodejs" ]
 then mkdir -p intouch_api/nodejs_client/
 	 curl http://newapi.nightly.capillary.in/swagger.json > swagger.json
