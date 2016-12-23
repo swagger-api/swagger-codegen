03ba912f0239d423932e94a5e38f0305291683fd
swativerma11 <swati.verma@capillarytech.com>
2016-12-23 11:16:36 +0530

    restrict skip overwriting

--- swagger_sdk_gen.sh
+++ swagger_sdk_gen.sh
@@ -4,7 +4,9 @@ fi
 curl -k "http://newapi.nightly.capillary.in/version.json" -o config.json
 echo "GENERATING SDK"
 if [ "$Client" = "java" ]
-then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+then 
+  rm -rf intouch_api/java_client/java
+  java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i http://newapi.nightly.capillary.in/swagger.json  \
   -l java \
   -DdateLibrary=java8 \
@@ -13,20 +15,23 @@ then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar genera
   tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
   mvn3 clean deploy -f intouch_api/java_client/java/pom.xml
 elif [ "$Client" = "c#" ]
-then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+then rm -rf intouch_api/csharp_client/c#
+   java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i http://newapi.nightly.capillary.in/swagger.json \
   -l csharp\
   -DtargetFramework=v$Version \
   -o intouch_api/csharp_client/c#
   tar cvzf intouch_api/csharp_client/c#swagger_sdK_$BUILD_NUMBER.tar.gz -C ./intouch_api/csharp_client/c#/ .
 elif [ "$Client" = "php" ]
-then java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+then rm -rf intouch_api/php_client/php
+   java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i http://newapi.nightly.capillary.in/swagger.json  \
   -l php \
   -o intouch_api/php_client/php
   tar cvzf intouch_api/php_client/php_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/php_client/php/ .
 elif [ "$Client" = "nodejs" ]
-then mkdir -p intouch_api/nodejs_client/
+then rm -rf intouch_api/nodejs_client
+	mkdir -p intouch_api/nodejs_client/
 	 curl http://newapi.nightly.capillary.in/swagger.json > swagger.json
      npm install swagger-js-codegen
      cd swagger-js-codegen
