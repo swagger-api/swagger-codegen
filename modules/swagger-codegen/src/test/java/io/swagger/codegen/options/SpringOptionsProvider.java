65384d5c057391d866c1d8df5bef43bdedf85f72
J. Fiala <johannes.fiala@fwd.at>
2017-01-06 18:39:59 +0100

    add tests for spring #4091

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/options/SpringOptionsProvider.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/SpringOptionsProvider.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package io.swagger.codegen.options;
 
 import io.swagger.codegen.CodegenConstants;
@@ -18,6 +19,7 @@ public class SpringOptionsProvider extends JavaOptionsProvider {
     public static final String ASYNC = "true";
     public static final String RESPONSE_WRAPPER = "Callable";
     public static final String USE_TAGS = "useTags";
+    public static final String USE_BEANVALIDATION = "false";
 
     @Override
     public String getLanguage() {
@@ -38,6 +40,7 @@ public class SpringOptionsProvider extends JavaOptionsProvider {
         options.put(SpringCodegen.ASYNC, ASYNC);
         options.put(SpringCodegen.RESPONSE_WRAPPER, RESPONSE_WRAPPER);
         options.put(SpringCodegen.USE_TAGS, USE_TAGS);
+        options.put(SpringCodegen.USE_BEANVALIDATION, USE_BEANVALIDATION);
 
         return options;
     }
