0d6f9d1f6b27af03452bb6d15f7a63a804ff7d80
dhuning <dennis.huning@audi.de>
2017-02-24 14:41:04 +0100

    Adds generated sample for #3819

---
+++ samples/server/petstore/springboot-useoptional/src/main/java/io/swagger/api/FakeApiController.java
@@ -0,0 +1,12 @@
+package io.swagger.api;
+
+import org.springframework.stereotype.Controller;
+
+import javax.validation.constraints.*;
+
+@Controller
+public class FakeApiController implements FakeApi {
+
+
+
+}
---
+++ samples/server/petstore/springboot-useoptional/src/main/java/io/swagger/api/StoreApiController.java
@@ -0,0 +1,12 @@
+package io.swagger.api;
+
+import org.springframework.stereotype.Controller;
+
+import javax.validation.constraints.*;
+
+@Controller
+public class StoreApiController implements StoreApi {
+
+
+
+}
