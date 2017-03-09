3d04107c6ce3c6334a4c6a4ab59cbfb1032f396b
Johannes Fiala <johannes.fiala@fwd.at>
2017-03-09 21:50:00 +0100

    dont compare swagger.json for now #4087

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/generators/EclipseFilenameFilter.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/generators/EclipseFilenameFilter.java
@@ -7,7 +7,8 @@ public class EclipseFilenameFilter implements java.io.FilenameFilter {
     @Override
     public boolean accept(File dir, String name) {
 
-        if (".settings".equals(name) || "target".equals(name) || ".classpath".equals(name) || ".project".equals(name)) {
+    	// TODO don't compare swagger.json for now
+        if (".settings".equals(name) || "target".equals(name) || ".classpath".equals(name) || ".project".equals(name) || "swagger.json".equals(name)) {
             return false;
         } else {
             return true;
