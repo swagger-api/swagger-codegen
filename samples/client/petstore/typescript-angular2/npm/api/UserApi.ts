c1b80afa2aec96286f2a143d679d087d25ee2916
wing328 <wing328hk@gmail.com>
2017-03-09 22:02:54 +0800

    update ts angular2 sample

--- samples/client/petstore/typescript-angular2/npm/api/UserApi.ts
+++ samples/client/petstore/typescript-angular2/npm/api/UserApi.ts
@@ -371,11 +371,11 @@ export class UserApi {
         let queryParameters = new URLSearchParams();
         let headers = new Headers(this.defaultHeaders.toJSON()); // https://github.com/angular/angular/issues/6845
         if (username !== undefined) {
-                    queryParameters.set('username', <any>username);
+            queryParameters.set('username', <any>username);
         }
 
         if (password !== undefined) {
-                    queryParameters.set('password', <any>password);
+            queryParameters.set('password', <any>password);
         }
 
         // to determine the Content-Type header
