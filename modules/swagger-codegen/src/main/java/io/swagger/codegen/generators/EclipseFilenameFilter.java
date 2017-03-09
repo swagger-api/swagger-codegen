b5676797892877a6b10b8a0f7fc809ddbeb94d6d
Johannes Fiala <johannes.fiala@fwd.at>
2017-03-09 21:59:41 +0100

    update comment #4087

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/generators/EclipseFilenameFilter.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/generators/EclipseFilenameFilter.java
@@ -7,8 +7,7 @@ public class EclipseFilenameFilter implements java.io.FilenameFilter {
     @Override
     public boolean accept(File dir, String name) {
 
-    	// TODO don't compare swagger.json for now
-        if (".settings".equals(name) || "target".equals(name) || ".classpath".equals(name) || ".project".equals(name) ) {
+    	if (".settings".equals(name) || "target".equals(name) || ".classpath".equals(name) || ".project".equals(name) ) {
             return false;
         } else {
             return true;
