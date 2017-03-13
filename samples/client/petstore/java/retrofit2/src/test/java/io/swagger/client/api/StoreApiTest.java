a8de22de05ce0da0a1dc62df144d3f37f641fb5a
Paŭlo Ebermann <paul@fencing-game.de>
2017-03-13 12:27:58 +0100

    Update samples for retrofit2.
    This includes deleting files which are not generated anymore (or with different name).

--- samples/client/petstore/java/retrofit2/src/test/java/io/swagger/client/api/StoreApiTest.java
+++ samples/client/petstore/java/retrofit2/src/test/java/io/swagger/client/api/StoreApiTest.java
@@ -1,75 +1,77 @@
 package io.swagger.client.api;
 
-import io.swagger.TestUtils;
-
 import io.swagger.client.ApiClient;
-import io.swagger.client.api.*;
-import io.swagger.client.model.*;
+import io.swagger.client.model.Order;
+import org.junit.Before;
+import org.junit.Test;
 
-import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.junit.*;
-
-import retrofit2.Response;
-import static org.junit.Assert.*;
-
+/**
+ * API tests for StoreApi
+ */
 public class StoreApiTest {
-    StoreApi api = null;
+
+    private StoreApi api;
 
     @Before
     public void setup() {
         api = new ApiClient().createService(StoreApi.class);
     }
 
+    
+    /**
+     * Delete purchase order by ID
+     *
+     * For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors
+     */
     @Test
-    public void testGetInventory() throws Exception {
-        Map<String, Integer> inventory = api.getInventory().execute().body();
-        assertTrue(inventory.keySet().size() > 0);
-    }
+    public void deleteOrderTest() {
+        String orderId = null;
+        // Void response = api.deleteOrder(orderId);
 
+        // TODO: test validations
+    }
+    
+    /**
+     * Returns pet inventories by status
+     *
+     * Returns a map of status codes to quantities
+     */
     @Test
-    public void testPlaceOrder() throws Exception {
-        Order order = createOrder();
-        api.placeOrder(order).execute();
+    public void getInventoryTest() {
+        // Map<String, Integer> response = api.getInventory();
 
-        Order fetched = api.getOrderById(order.getId()).execute().body();
-        assertEquals(order.getId(), fetched.getId());
-        assertEquals(order.getPetId(), fetched.getPetId());
-        assertEquals(order.getQuantity(), fetched.getQuantity());
-        assertEquals(order.getShipDate().toInstant(), fetched.getShipDate().toInstant());
+        // TODO: test validations
     }
-
+    
+    /**
+     * Find purchase order by ID
+     *
+     * For valid response try integer IDs with value &lt;&#x3D; 5 or &gt; 10. Other values will generated exceptions
+     */
     @Test
-    public void testDeleteOrder() throws Exception {
-        Order order = createOrder();
-        Response<Order> aa = api.placeOrder(order).execute();
+    public void getOrderByIdTest() {
+        Long orderId = null;
+        // Order response = api.getOrderById(orderId);
 
-        Order fetched = api.getOrderById(order.getId()).execute().body();
-        assertEquals(fetched.getId(), order.getId());
-
-        api.deleteOrder(String.valueOf(order.getId())).execute();
-
-        api.getOrderById(order.getId()).execute();
-        //also in retrofit 1 should return an error but don't, check server api impl.
+        // TODO: test validations
     }
+    
+    /**
+     * Place an order for a pet
+     *
+     * 
+     */
+    @Test
+    public void placeOrderTest() {
+        Order body = null;
+        // Order response = api.placeOrder(body);
 
-    private Order createOrder() {
-        Order order = new Order();
-        order.setPetId(new Long(200));
-        order.setQuantity(new Integer(13));
-        order.setShipDate(org.joda.time.DateTime.now());
-        order.setStatus(Order.StatusEnum.PLACED);
-        order.setComplete(true);
-
-        try {
-            Field idField = Order.class.getDeclaredField("id");
-            idField.setAccessible(true);
-            idField.set(order, TestUtils.nextId());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return order;
+        // TODO: test validations
     }
+    
 }
