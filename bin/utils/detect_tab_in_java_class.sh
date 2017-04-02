4eb09f17e473e2273096a535f71208360e6479f3
wing328 <wing328hk@gmail.com>
2017-04-02 15:13:55 +0800

    print result before checking

--- bin/utils/detect_tab_in_java_class.sh
+++ bin/utils/detect_tab_in_java_class.sh
@@ -3,9 +3,10 @@
 # grep for \t in the generators
 RESULT=`find modules/swagger-codegen/src/ -name "*.java" | xargs grep "\t"`
 
+echo -e "$RESULT"
+
 if [ "$RESULT" != "" ]; then
     echo "Java files contain tab '\\t'. Please remove it and try again."
-    echo $RESULT
     exit 1;
 fi
 
