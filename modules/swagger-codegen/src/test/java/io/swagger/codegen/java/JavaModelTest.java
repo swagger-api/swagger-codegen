b19bada40b63b63fb6423a4feb386a955dd402da
Egor Masalitin <emasalitin@gmail.com>
2017-04-06 13:22:34 +0300

    Test for modelNullValues null was added

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/java/JavaModelTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/java/JavaModelTest.java
@@ -116,20 +116,31 @@ public class JavaModelTest {
     }
 
     @Test(description = "convert a model when modelDefaultNull=true")
-    public void modelDefaultNullParamTest() {
+    public void modelDefaultNullTrueParamTest() {
         final Model model = new ModelImpl()
-                .description("a sample model")
                 .property("urls", new ArrayProperty()
                         .items(new StringProperty()));
         final JavaClientCodegen codegen = new JavaClientCodegen();
         codegen.setModelDefaultNull(true);
         final CodegenModel cm = codegen.fromModel("sample", model);
 
-        Assert.assertEquals(cm.vars.size(), 1);
-
         final CodegenProperty property = cm.vars.get(0);
         Assert.assertEquals(property.datatype, "List<String>");
         Assert.assertEquals(property.defaultValue, "null");
+    }
+
+    @Test(description = "convert a model when modelDefaultNull=false")
+    public void modelDefaultNullFalseParamTest() {
+        final Model model = new ModelImpl()
+                .property("urls", new ArrayProperty()
+                        .items(new StringProperty()));
+        final JavaClientCodegen codegen = new JavaClientCodegen();
+        codegen.setModelDefaultNull(false);
+        final CodegenModel cm = codegen.fromModel("sample", model);
+
+        final CodegenProperty property = cm.vars.get(0);
+        Assert.assertEquals(property.datatype, "List<String>");
+        Assert.assertEquals(property.defaultValue, "new ArrayList<String>()");
     }
 
     @Test(description = "convert a model with a map property")
