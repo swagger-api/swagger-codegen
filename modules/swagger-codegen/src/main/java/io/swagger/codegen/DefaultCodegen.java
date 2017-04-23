2fd604d067917301d63aef860570a4b93b6e5f34
Nick Bruno <nbruno@users.noreply.github.com>
2017-04-22 23:41:28 -0400

    Enable Tag details to be used in templates
    Adds the ability to reference the tag name, description, external
    docs, and vendor extensions in Mustache templates during codegen

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
@@ -1960,7 +1960,6 @@ public class DefaultCodegen {
         op.summary = escapeText(operation.getSummary());
         op.unescapedNotes = operation.getDescription();
         op.notes = escapeText(operation.getDescription());
-        op.tags = operation.getTags();
         op.hasConsumes = false;
         op.hasProduces = false;
 
