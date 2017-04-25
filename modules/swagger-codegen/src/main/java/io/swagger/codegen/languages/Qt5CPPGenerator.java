7014f8fcd71083a5d5feb6767e66d09eabf76716
sabras75 <sabras75@gmail.com>
2017-04-25 10:24:21 +0200

    Simplify and use a simple QString instead of a QUuid. Work for using a QUuid is way larger than supporting QString (imply modifying multiple c++ templates). Current needs are fullfill with the use of a simple QString, so I let the use of QUuid for future enhancement.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/Qt5CPPGenerator.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/Qt5CPPGenerator.java
@@ -126,7 +126,11 @@ public class Qt5CPPGenerator extends DefaultCodegen implements CodegenConfig {
         // mapped to String as a workaround
         typeMapping.put("binary", "QString");
         typeMapping.put("ByteArray", "QByteArray");
-        typeMapping.put("UUID", "QUuid");
+        // UUID support - possible enhancement : use QUuid instead of QString.
+        //   beware though that Serialisation/deserialisation of QUuid does not
+        //   come out of the box and will need to be sorted out (at least imply
+        //   modifications on multiple templates)
+        typeMapping.put("UUID", "QString");
 
         importMapping = new HashMap<String, String>();
 
@@ -142,7 +146,6 @@ public class Qt5CPPGenerator extends DefaultCodegen implements CodegenConfig {
         systemIncludes.add("QDate");
         systemIncludes.add("QDateTime");
         systemIncludes.add("QByteArray");
-        systemIncludes.add("QUuid");
     }
 
     /**
