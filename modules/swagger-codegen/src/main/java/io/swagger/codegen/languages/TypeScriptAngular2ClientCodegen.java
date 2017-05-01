bd9d5f4fbe88aa8c3002f7eb92efe135a9e3a170
Ian White <ibwhite@gmail.com>
2017-04-30 20:21:51 -0400

    ensuring that swagger dates are treated as strings for all typescript clients

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngular2ClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngular2ClientCodegen.java
@@ -42,7 +42,6 @@ public class TypeScriptAngular2ClientCodegen extends AbstractTypeScriptClientCod
         modelTemplateFiles.put("model.mustache", ".ts");
         apiTemplateFiles.put("api.service.mustache", ".ts");
         languageSpecificPrimitives.add("Blob");
-        typeMapping.put("date","string");
         typeMapping.put("file","Blob");
         apiPackage = "api";
         modelPackage = "model";
