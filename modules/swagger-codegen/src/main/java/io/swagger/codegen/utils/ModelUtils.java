3c350f6dbe41e9062de2b4007bbe1ccc8b635a87
harishchoragudi <harish.choragudi@gmail.com>
2017-06-05 12:58:30 -0500

    cpprest models now use inheritance properly instead of always extending from ModelBase

---
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/utils/ModelUtils.java
@@ -0,0 +1,29 @@
+package io.swagger.codegen.utils;
+
+import io.swagger.codegen.CodegenModel;
+
+import java.util.List;
+import java.util.Map;
+
+public class ModelUtils {
+    public static CodegenModel getModelByName(final String name, final Map<String, Object> models) {
+        final Object data = models.get(name);
+        if (data instanceof Map) {
+            final Map<?, ?> dataMap = (Map<?, ?>) data;
+            final Object dataModels = dataMap.get("models");
+            if (dataModels instanceof List) {
+                final List<?> dataModelsList = (List<?>) dataModels;
+                for (final Object entry : dataModelsList) {
+                    if (entry instanceof Map) {
+                        final Map<?, ?> entryMap = (Map<?, ?>) entry;
+                        final Object model = entryMap.get("model");
+                        if (model instanceof CodegenModel) {
+                            return (CodegenModel) model;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
