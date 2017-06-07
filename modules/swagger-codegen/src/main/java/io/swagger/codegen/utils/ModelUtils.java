0b088114d6057de0a4b00ecdb5066d2f3bc0950b
harishchoragudi <harish.choragudi@gmail.com>
2017-06-07 12:58:56 -0500

    corrected the javadoc

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/utils/ModelUtils.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/utils/ModelUtils.java
@@ -7,10 +7,11 @@ import java.util.Map;
 
 public class ModelUtils {
     /**
+     * Searches for the model by name in the map of models and returns it
      *
      * @Param name Name of the model
      * @Param models Map of models
-     * @return Searches for the model by name in the map of models and returns it
+     * @return model
      */
     public static CodegenModel getModelByName(final String name, final Map<String, Object> models) {
         final Object data = models.get(name);
