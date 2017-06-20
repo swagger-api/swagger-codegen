d4f46cf41be9449f860d76b6385f5ef9aac76fa0
wing328 <wing328hk@gmail.com>
2017-06-20 16:13:09 +0800

    clean up powershell comment, update model/api test files

--- samples/client/petstore/powershell_test/test/ApiResponseTest.ps1
+++ samples/client/petstore/powershell_test/test/ApiResponseTest.ps1
@@ -1,27 +1,2 @@
 ## TODO we need to update the template to test the model files
 
-function New-ApiResponse {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Position = 0, ValueFromPipelineByPropertyName = $true)]
-        [System.Nullable[Int32]]
-        ${code},
-        [Parameter(Position = 0, ValueFromPipelineByPropertyName = $true)]
-        [String]
-        ${type},
-        [Parameter(Position = 0, ValueFromPipelineByPropertyName = $true)]
-        [String]
-        ${message}
-    )
-
-    Process {
-        'Creating object: IO.Swagger.Model.ApiResponse' | Write-Verbose
-        $PSBoundParameters | Out-DebugParameter | Write-Debug
-
-        New-Object -TypeName IO.Swagger.Model.ApiResponse -ArgumentList @(
-            ${code},
-            ${type},
-            ${message}
-        )
-    }
-}
--- samples/client/petstore/powershell_test/test/CategoryTest.ps1
+++ samples/client/petstore/powershell_test/test/CategoryTest.ps1
@@ -1,23 +1,2 @@
 ## TODO we need to update the template to test the model files
 
-function New-Category {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Position = 0, ValueFromPipelineByPropertyName = $true)]
-        [System.Nullable[Int64]]
-        ${id},
-        [Parameter(Position = 0, ValueFromPipelineByPropertyName = $true)]
-        [String]
-        ${name}
-    )
-
-    Process {
-        'Creating object: IO.Swagger.Model.Category' | Write-Verbose
-        $PSBoundParameters | Out-DebugParameter | Write-Debug
-
-        New-Object -TypeName IO.Swagger.Model.Category -ArgumentList @(
-            ${id},
-            ${name}
-        )
-    }
-}
