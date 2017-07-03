f19e9d4a984b4542e0903b18d684df26e6015a69
wing328 <wing328hk@gmail.com>
2017-07-03 16:44:54 +0800

    rpelace tab with 4-space

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/features/OptionalFeatures.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/features/OptionalFeatures.java
@@ -2,9 +2,9 @@ package io.swagger.codegen.languages.features;
 
 public interface OptionalFeatures {
 
-	// Language supports generating Optional Types
-	String USE_OPTIONAL = "useOptional";
+    // Language supports generating Optional Types
+    String USE_OPTIONAL = "useOptional";
 
-	void setUseOptional(boolean useOptional);
+    void setUseOptional(boolean useOptional);
 
 }
