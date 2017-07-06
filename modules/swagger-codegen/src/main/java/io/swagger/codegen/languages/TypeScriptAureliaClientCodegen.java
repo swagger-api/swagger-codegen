253e6be448e18686f39e61d0d8aa0de8f73b12ca
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2017-07-06 14:50:35 +0200

    Implement authentication methods for Aurelia #5987

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAureliaClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAureliaClientCodegen.java
@@ -45,6 +45,7 @@ public class TypeScriptAureliaClientCodegen extends AbstractTypeScriptClientCode
         // Set supporting files
         supportingFiles.add(new SupportingFile("models.mustache", "", "models.ts"));
         supportingFiles.add(new SupportingFile("Api.ts.mustache", "", "Api.ts"));
+        supportingFiles.add(new SupportingFile("AuthStorage.ts.mustache", "", "AuthStorage.ts"));
         supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
         supportingFiles.add(new SupportingFile("README.md", "", "README.md"));
         supportingFiles.add(new SupportingFile("package.json.mustache", "", "package.json"));
