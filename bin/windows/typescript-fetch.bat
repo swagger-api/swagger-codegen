5231d8edca3b7baa5013c0dc8257958fb29455fd
Isman Usoh <isman.usoh@gmail.com>
2017-07-12 00:48:56 +0700

    new typescript-fetch code base on typescript-angular2

--- bin/windows/typescript-fetch.bat
+++ bin/windows/typescript-fetch.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
-set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.yaml -l typescript-fetch -o samples\client\petstore\typescript-fetch
+set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.yaml -l typescript-fetch -o samples\client\petstore\typescript-fetch\default
 
 java %JAVA_OPTS% -jar %executable% %ags%
