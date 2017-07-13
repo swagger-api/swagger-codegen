bde669b90cee6fa253ee5ef0cc9e85afa605b09d
Isman Usoh <isman.usoh@gmail.com>
2017-07-13 19:43:05 +0700

    Fix

--- bin/security/windows/typescript-fetch-petstore.bat
+++ bin/security/windows/typescript-fetch-petstore.bat
@@ -5,6 +5,6 @@ If Not Exist %executable% (
 )
 
 REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
-set ags=generate -t modules/swagger-codegen/src/main/resources/typescript-fetch -i modules/swagger-codegen/src/test/resources/2_0/petstore-security-test.yaml -l typescript-fetch -o samples/client/petstore-security-test/typescript-fetch
+set ags=generate -t modules\swagger-codegen\src\main\resources\typescript-fetch -i modules\swagger-codegen\src\test\resources\2_0\petstore-security-test.yaml -l typescript-fetch -o samples\client\petstore-security-test\typescript-fetch
 
 java %JAVA_OPTS% -jar %executable% %ags%
--- bin/windows/typescript-fetch-petstore.bat
+++ bin/windows/typescript-fetch-petstore.bat
@@ -5,13 +5,13 @@ If Not Exist %executable% (
 )
 
 echo "Typescript Petstore API client (default)"
-set ags=generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l typescript-fetch -o samples/client/petstore/typescript-fetch/default"
+set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.yaml -l typescript-fetch -o samples\client\petstore\typescript-fetch\default"
 java %JAVA_OPTS% -jar %executable% %ags%
 
 echo "Typescript Petstore API client (with interfaces generated)"
-set ags=generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l typescript-fetch -o samples/client/petstore/typescript-fetch/with-interfaces -D withInterfaces=true"
+set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.yaml -l typescript-fetch -o samples\client\petstore\typescript-fetch\with-interfaces -D withInterfaces=true"
 java %JAVA_OPTS% -jar %executable% %ags%
 
 echo "Typescript Petstore API client (npm setting)"
-set ags=generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l typescript-fetch -c bin/typescript-fetch-petstore-with-npm.json -o samples/client/petstore/typescript-fetch/npm"
+set ags=generate -i modules\swagger-codegen\src\test\resources\2_0\petstore.yaml -l typescript-fetch -c bin\typescript-fetch-petstore-with-npm.json -o samples\client\petstore\typescript-fetch\npm"
 java %JAVA_OPTS% -jar %executable% %ags%
