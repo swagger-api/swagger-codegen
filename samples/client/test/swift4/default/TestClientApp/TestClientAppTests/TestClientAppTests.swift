61dff90c71889cedb8c8cd4711e601f1b54a8955
Eric Hyche <ehyche@groupon.com>
2017-07-19 10:31:41 -0400

    Add test app for swift4Test.json schema

---
+++ samples/client/test/swift4/default/TestClientApp/TestClientAppTests/TestClientAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestClientAppTests.swift
+//  TestClientAppTests
+//
+//  Created by Eric Hyche on 7/18/17.
+//  Copyright © 2017 Swagger Codegen. All rights reserved.
+//
+
+import XCTest
+@testable import TestClientApp
+
+class TestClientAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
