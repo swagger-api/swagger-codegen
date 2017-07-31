15ef1057b26ee057b3834f5a3fbaf05327ade63a
swativerma11 <swati.verma@capillarytech.com>
2017-07-31 18:09:47 +0530

    cluster wise sdk generation

--- swagger_sdk_gen.sh
+++ swagger_sdk_gen.sh
@@ -1,7 +1,32 @@
 if $Build_Codegen ; then
     mvn3 clean package -Dmaven.test.skip=true
 fi
-curl -k "http://newapi.staging.capillary.in/version.json" -o config.json
+if [ "$Environment" = "Nightly" ]
+  then
+    $url = "http://newapi.nightly.capillary.in/swagger.json"
+    $version ="http://newapi.nightly.capillary.in/version.json"
+elif ["$Environment" = "Staging"]
+  then
+    $url = "http://newapi.staging.capillary.in/swagger.json"
+    $version ="http://newapi.staging.capillary.in/version.json"
+elif ["$Environment" = "APAC2"]
+  then
+elif ["$Environment" = "INDIA"]
+  then
+  $url ="http://newapi.capillary.co.in/swagger.json"
+  $version ="http://newapi.capillary.co.in/version.json"
+elif ["$Environment" = "CHINA"]
+  then 
+      $url = "http://newapi.capillarytech.cn.com/swagger.json"
+      $version ="http://http://newapi.capillarytech.cn.com/version.json"
+elif ["$Environment" = "EU"]
+  then
+  $url = "http://newapi.capillarytech.cn.com/swagger.json"
+  $version ="http://http://newapi.eu.capillarytech.com/version.json"
+else " No Environment is selected"
+fi
+  #statements
+curl -k $version -o config.json
 echo "GENERATING SDK"
 if [ "$Client" = "java" ]
 then 
@@ -25,7 +50,7 @@ then rm -rf intouch_api/csharp_client/c#
 elif [ "$Client" = "php" ]
 then rm -rf intouch_api/php_client/php
    java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-  -i http://newapi.staging.capillary.in/swagger.json  \
+  -i $url  \
   -l php \
   -o intouch_api/php_client/php
   tar cvzf intouch_api/php_client/php_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/php_client/php/ .
