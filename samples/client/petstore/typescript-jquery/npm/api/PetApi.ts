a67ed1639cf33f32b28415fd25aac5965e96cfed
Jean-François Côté <jcote@stingray.com>
2017-08-08 13:59:00 -0400

    Prevent unnecessary append to the FormData

--- samples/client/petstore/typescript-jquery/npm/api/PetApi.ts
+++ samples/client/petstore/typescript-jquery/npm/api/PetApi.ts
@@ -433,11 +433,9 @@ export class PetApi {
         if (name !== null && name !== undefined) {
             formParams.append('name', <any>name);
         }
-
         if (status !== null && status !== undefined) {
             formParams.append('status', <any>status);
         }
-
         // to determine the Content-Type header
         let consumes: string[] = [
             'application/x-www-form-urlencoded'
@@ -515,13 +513,8 @@ export class PetApi {
         if (additionalMetadata !== null && additionalMetadata !== undefined) {
             formParams.append('additionalMetadata', <any>additionalMetadata);
         }
-
         reqHasFile = true;
         formParams = file;
-        if (file !== null && file !== undefined) {
-            formParams.append('file', <any>file);
-        }
-
         // to determine the Content-Type header
         let consumes: string[] = [
             'multipart/form-data'
