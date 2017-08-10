95bd5b7b1edd1776a2ce8a866d7aa88e1a7845f7
wing328 <wing328hk@gmail.com>
2017-08-10 21:59:28 +0800

    skip auto-generated readme, doc, test files

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/LuaClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/LuaClientCodegen.java
@@ -141,10 +141,16 @@ public class LuaClientCodegen extends DefaultCodegen implements CodegenConfig {
         additionalProperties.put("apiDocPath", apiDocPath);
         additionalProperties.put("modelDocPath", modelDocPath);
 
+        apiTestTemplateFiles.clear(); // TODO: add api test template
+        modelTestTemplateFiles.clear(); // TODO: add model test template
+
+        apiDocTemplateFiles.clear(); // TODO: add api doc template
+        modelDocTemplateFiles.clear(); // TODO: add model doc template
+
         modelPackage = packageName;
         apiPackage = packageName;
 
-        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        //supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
         supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
         supportingFiles.add(new SupportingFile("gitignore.mustache", "", ".gitignore"));
         //supportingFiles.add(new SupportingFile("configuration.mustache", "", "configuration.lua"));
