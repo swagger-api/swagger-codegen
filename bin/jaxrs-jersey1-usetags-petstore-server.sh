302d486c555e7a58f8e906de23e0ba957cc84105
wing328 <wing328hk@gmail.com>
2017-08-12 18:39:43 +0800

    fix duplicated id

--- bin/jaxrs-jersey1-usetags-petstore-server.sh
+++ bin/jaxrs-jersey1-usetags-petstore-server.sh
@@ -29,6 +29,6 @@ export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/
 ags="$@ generate -t modules/swagger-codegen/src/main/resources/JavaJaxRS/libraries/jersey1 -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l jaxrs -o samples/server/petstore/jaxrs/jersey1-useTags -DhideGenerationTimestamp=true,serverPort=8080 --library=jersey1 --artifact-id=swagger-jaxrs-jersey1-useTags --additional-properties useTags=true"
 
 echo "Removing files and folders under samples/server/petstore/jaxrs/jersey1-useTags/src/main"
-rm -rf samples/server/petstore/jaxrs/jersey1/src/main
-find samples/server/petstore/jaxrs/jersey1 -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
+rm -rf samples/server/petstore/jaxrs/jersey1-useTags/src/main
+find samples/server/petstore/jaxrs/jersey1-useTags -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
 java $JAVA_OPTS -jar $executable $ags
--- bin/jaxrs-usetags-petstore-server.sh
+++ bin/jaxrs-usetags-petstore-server.sh
@@ -29,6 +29,6 @@ export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/
 ags="$@ generate -t modules/swagger-codegen/src/main/resources/JavaJaxRS -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l jaxrs -o samples/server/petstore/jaxrs/jersey2-useTags -DhideGenerationTimestamp=true,serverPort=8080 --artifact-id=swagger-jaxrs-jersey2-useTags --additional-properties useTags=true"
 
 echo "Removing files and folders under samples/server/petstore/jaxrs/jersey2-useTags/src/main"
-rm -rf samples/server/petstore/jaxrs/jersey2/src/main
-find samples/server/petstore/jaxrs/jersey2 -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
+rm -rf samples/server/petstore/jaxrs/jersey2-useTags/src/main
+find samples/server/petstore/jaxrs/jersey2-useTags -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
 java $JAVA_OPTS -jar $executable $ags
