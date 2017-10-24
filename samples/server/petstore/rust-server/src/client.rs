6b94d7231e4f421d1eea820daf8ad318f5fb95b0
Nigel Thorpe <nigel.thorpe@metaswitch.com>
2017-10-24 10:07:25 +0100

    Percent-encode path and query parameters in client URLs Fixes #122
    Also don't include a question mark at the end of the path when there are no query paramters.
    Fixes #121

--- samples/server/petstore/rust-server/src/client.rs
+++ samples/server/petstore/rust-server/src/client.rs
@@ -1,6 +1,7 @@
 #![allow(unused_extern_crates)]
 extern crate hyper_openssl;
 extern crate chrono;
+extern crate url;
 extern crate multipart;
 
 use multipart::client::lazy::Multipart;
@@ -11,6 +12,7 @@ use hyper::header::{Headers, ContentType};
 use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 use hyper::Url;
 use self::hyper_openssl::openssl;
+use self::url::percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET, QUERY_ENCODE_SET};
 use futures;
 use futures::{Future, Stream};
 use futures::{future, stream};
@@ -200,7 +202,10 @@ impl Api for Client {
     fn test_special_tags(&self, param_body: models::Client, context: &Context) -> Box<Future<Item=TestSpecialTagsResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/another-fake/dummy?", self.base_path);
+        let url = format!(
+            "{}/v2/another-fake/dummy",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -253,7 +258,10 @@ impl Api for Client {
     fn fake_outer_boolean_serialize(&self, param_body: Option<models::OuterBoolean>, context: &Context) -> Box<Future<Item=FakeOuterBooleanSerializeResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake/outer/boolean?", self.base_path);
+        let url = format!(
+            "{}/v2/fake/outer/boolean",
+            self.base_path
+        );
 
         let body = param_body.map(|ref body| {
 
@@ -310,7 +318,10 @@ impl Api for Client {
     fn fake_outer_composite_serialize(&self, param_body: Option<models::OuterComposite>, context: &Context) -> Box<Future<Item=FakeOuterCompositeSerializeResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake/outer/composite?", self.base_path);
+        let url = format!(
+            "{}/v2/fake/outer/composite",
+            self.base_path
+        );
 
         let body = param_body.map(|ref body| {
 
@@ -367,7 +378,10 @@ impl Api for Client {
     fn fake_outer_number_serialize(&self, param_body: Option<models::OuterNumber>, context: &Context) -> Box<Future<Item=FakeOuterNumberSerializeResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake/outer/number?", self.base_path);
+        let url = format!(
+            "{}/v2/fake/outer/number",
+            self.base_path
+        );
 
         let body = param_body.map(|ref body| {
 
@@ -424,7 +438,10 @@ impl Api for Client {
     fn fake_outer_string_serialize(&self, param_body: Option<models::OuterString>, context: &Context) -> Box<Future<Item=FakeOuterStringSerializeResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake/outer/string?", self.base_path);
+        let url = format!(
+            "{}/v2/fake/outer/string",
+            self.base_path
+        );
 
         let body = param_body.map(|ref body| {
 
@@ -481,7 +498,10 @@ impl Api for Client {
     fn test_client_model(&self, param_body: models::Client, context: &Context) -> Box<Future<Item=TestClientModelResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake?", self.base_path);
+        let url = format!(
+            "{}/v2/fake",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -534,7 +554,10 @@ impl Api for Client {
     fn test_endpoint_parameters(&self, param_number: f64, param_double: f64, param_pattern_without_delimiter: String, param_byte: swagger::ByteArray, param_integer: Option<i32>, param_int32: Option<i32>, param_int64: Option<i64>, param_float: Option<f32>, param_string: Option<String>, param_binary: Option<swagger::ByteArray>, param_date: Option<chrono::DateTime<chrono::Utc>>, param_date_time: Option<chrono::DateTime<chrono::Utc>>, param_password: Option<String>, param_callback: Option<String>, context: &Context) -> Box<Future<Item=TestEndpointParametersResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake?", self.base_path);
+        let url = format!(
+            "{}/v2/fake",
+            self.base_path
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -588,7 +611,13 @@ impl Api for Client {
         let query_enum_query_integer = param_enum_query_integer.map_or_else(String::new, |query| format!("enum_query_integer={enum_query_integer}&", enum_query_integer=query.to_string()));
 
 
-        let url = format!("{}/v2/fake?{enum_query_string_array}{enum_query_string}{enum_query_integer}", self.base_path, enum_query_string_array=query_enum_query_string_array, enum_query_string=query_enum_query_string, enum_query_integer=query_enum_query_integer);
+        let url = format!(
+            "{}/v2/fake?{enum_query_string_array}{enum_query_string}{enum_query_integer}",
+            self.base_path,
+            enum_query_string_array=utf8_percent_encode(&query_enum_query_string_array, QUERY_ENCODE_SET),
+            enum_query_string=utf8_percent_encode(&query_enum_query_string, QUERY_ENCODE_SET),
+            enum_query_integer=utf8_percent_encode(&query_enum_query_integer, QUERY_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -692,7 +721,10 @@ impl Api for Client {
     fn test_json_form_data(&self, param_param: String, param_param2: String, context: &Context) -> Box<Future<Item=TestJsonFormDataResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake/jsonFormData?", self.base_path);
+        let url = format!(
+            "{}/v2/fake/jsonFormData",
+            self.base_path
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -736,7 +768,10 @@ impl Api for Client {
     fn test_classname(&self, param_body: models::Client, context: &Context) -> Box<Future<Item=TestClassnameResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/fake_classname_test?", self.base_path);
+        let url = format!(
+            "{}/v2/fake_classname_test",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -789,7 +824,10 @@ impl Api for Client {
     fn add_pet(&self, param_body: models::Pet, context: &Context) -> Box<Future<Item=AddPetResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/pet?", self.base_path);
+        let url = format!(
+            "{}/v2/pet",
+            self.base_path
+        );
 
 
         let body = serde_xml_rs::to_string(&param_body).expect("impossible to fail to serialize");
@@ -838,7 +876,10 @@ impl Api for Client {
     fn delete_pet(&self, param_pet_id: i64, param_api_key: Option<String>, context: &Context) -> Box<Future<Item=DeletePetResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/pet/{petId}?", self.base_path, petId=param_pet_id.to_string());
+        let url = format!(
+            "{}/v2/pet/{petId}",
+            self.base_path, petId=utf8_percent_encode(&param_pet_id.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -889,7 +930,11 @@ impl Api for Client {
         let query_status = format!("status={status}&", status=param_status.join(","));
 
 
-        let url = format!("{}/v2/pet/findByStatus?{status}", self.base_path, status=query_status);
+        let url = format!(
+            "{}/v2/pet/findByStatus?{status}",
+            self.base_path,
+            status=utf8_percent_encode(&query_status, QUERY_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -947,7 +992,11 @@ impl Api for Client {
         let query_tags = format!("tags={tags}&", tags=param_tags.join(","));
 
 
-        let url = format!("{}/v2/pet/findByTags?{tags}", self.base_path, tags=query_tags);
+        let url = format!(
+            "{}/v2/pet/findByTags?{tags}",
+            self.base_path,
+            tags=utf8_percent_encode(&query_tags, QUERY_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1002,7 +1051,10 @@ impl Api for Client {
     fn get_pet_by_id(&self, param_pet_id: i64, context: &Context) -> Box<Future<Item=GetPetByIdResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/pet/{petId}?", self.base_path, petId=param_pet_id.to_string());
+        let url = format!(
+            "{}/v2/pet/{petId}",
+            self.base_path, petId=utf8_percent_encode(&param_pet_id.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1062,7 +1114,10 @@ impl Api for Client {
     fn update_pet(&self, param_body: models::Pet, context: &Context) -> Box<Future<Item=UpdatePetResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/pet?", self.base_path);
+        let url = format!(
+            "{}/v2/pet",
+            self.base_path
+        );
 
 
         let body = serde_xml_rs::to_string(&param_body).expect("impossible to fail to serialize");
@@ -1121,7 +1176,10 @@ impl Api for Client {
     fn update_pet_with_form(&self, param_pet_id: i64, param_name: Option<String>, param_status: Option<String>, context: &Context) -> Box<Future<Item=UpdatePetWithFormResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/pet/{petId}?", self.base_path, petId=param_pet_id.to_string());
+        let url = format!(
+            "{}/v2/pet/{petId}",
+            self.base_path, petId=utf8_percent_encode(&param_pet_id.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1165,12 +1223,15 @@ impl Api for Client {
     fn upload_file(&self, param_pet_id: i64, param_additional_metadata: Option<String>, param_file: Box<Future<Item=Option<Box<Stream<Item=Vec<u8>, Error=Error> + Send>>, Error=Error> + Send>, context: &Context) -> Box<Future<Item=UploadFileResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/pet/{petId}/uploadImage?", self.base_path, petId=param_pet_id.to_string());
+        let url = format!(
+            "{}/v2/pet/{petId}/uploadImage",
+            self.base_path, petId=utf8_percent_encode(&param_pet_id.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
         // Form data body
         let mut multipart = Multipart::new();
 
-        if let Ok(Some(param_file)) = param_file.wait() { 
+        if let Ok(Some(param_file)) = param_file.wait() {
             match convert_stream_to_string(param_file) {
                 Ok(param_file) => {
                     // Add file to multipart form.
@@ -1249,7 +1310,10 @@ impl Api for Client {
     fn delete_order(&self, param_order_id: String, context: &Context) -> Box<Future<Item=DeleteOrderResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/store/order/{order_id}?", self.base_path, order_id=param_order_id.to_string());
+        let url = format!(
+            "{}/v2/store/order/{order_id}",
+            self.base_path, order_id=utf8_percent_encode(&param_order_id.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1298,7 +1362,10 @@ impl Api for Client {
     fn get_inventory(&self, context: &Context) -> Box<Future<Item=GetInventoryResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/store/inventory?", self.base_path);
+        let url = format!(
+            "{}/v2/store/inventory",
+            self.base_path
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1346,7 +1413,10 @@ impl Api for Client {
     fn get_order_by_id(&self, param_order_id: i64, context: &Context) -> Box<Future<Item=GetOrderByIdResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/store/order/{order_id}?", self.base_path, order_id=param_order_id.to_string());
+        let url = format!(
+            "{}/v2/store/order/{order_id}",
+            self.base_path, order_id=utf8_percent_encode(&param_order_id.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1406,7 +1476,10 @@ impl Api for Client {
     fn place_order(&self, param_body: models::Order, context: &Context) -> Box<Future<Item=PlaceOrderResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/store/order?", self.base_path);
+        let url = format!(
+            "{}/v2/store/order",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -1466,7 +1539,10 @@ impl Api for Client {
     fn create_user(&self, param_body: models::User, context: &Context) -> Box<Future<Item=CreateUserResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user?", self.base_path);
+        let url = format!(
+            "{}/v2/user",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -1515,7 +1591,10 @@ impl Api for Client {
     fn create_users_with_array_input(&self, param_body: &Vec<models::User>, context: &Context) -> Box<Future<Item=CreateUsersWithArrayInputResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user/createWithArray?", self.base_path);
+        let url = format!(
+            "{}/v2/user/createWithArray",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -1564,7 +1643,10 @@ impl Api for Client {
     fn create_users_with_list_input(&self, param_body: &Vec<models::User>, context: &Context) -> Box<Future<Item=CreateUsersWithListInputResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user/createWithList?", self.base_path);
+        let url = format!(
+            "{}/v2/user/createWithList",
+            self.base_path
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
@@ -1613,7 +1695,10 @@ impl Api for Client {
     fn delete_user(&self, param_username: String, context: &Context) -> Box<Future<Item=DeleteUserResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user/{username}?", self.base_path, username=param_username.to_string());
+        let url = format!(
+            "{}/v2/user/{username}",
+            self.base_path, username=utf8_percent_encode(&param_username.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1662,7 +1747,10 @@ impl Api for Client {
     fn get_user_by_name(&self, param_username: String, context: &Context) -> Box<Future<Item=GetUserByNameResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user/{username}?", self.base_path, username=param_username.to_string());
+        let url = format!(
+            "{}/v2/user/{username}",
+            self.base_path, username=utf8_percent_encode(&param_username.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1726,7 +1814,12 @@ impl Api for Client {
         let query_password = format!("password={password}&", password=param_password.to_string());
 
 
-        let url = format!("{}/v2/user/login?{username}{password}", self.base_path, username=query_username, password=query_password);
+        let url = format!(
+            "{}/v2/user/login?{username}{password}",
+            self.base_path,
+            username=utf8_percent_encode(&query_username, QUERY_ENCODE_SET),
+            password=utf8_percent_encode(&query_password, QUERY_ENCODE_SET)
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1785,7 +1878,10 @@ impl Api for Client {
     fn logout_user(&self, context: &Context) -> Box<Future<Item=LogoutUserResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user/logout?", self.base_path);
+        let url = format!(
+            "{}/v2/user/logout",
+            self.base_path
+        );
 
 
         let hyper_client = (self.hyper_client)();
@@ -1829,7 +1925,10 @@ impl Api for Client {
     fn update_user(&self, param_username: String, param_body: models::User, context: &Context) -> Box<Future<Item=UpdateUserResponse, Error=ApiError> + Send> {
 
 
-        let url = format!("{}/v2/user/{username}?", self.base_path, username=param_username.to_string());
+        let url = format!(
+            "{}/v2/user/{username}",
+            self.base_path, username=utf8_percent_encode(&param_username.to_string(), PATH_SEGMENT_ENCODE_SET)
+        );
 
 
         let body = serde_json::to_string(&param_body).expect("impossible to fail to serialize");
