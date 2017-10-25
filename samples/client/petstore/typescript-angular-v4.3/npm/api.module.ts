5028c00b6872b8e8758e88251d15c1c1b03599d5
k26475 <kenneth.steward@finra.org>
2017-10-25 10:42:13 -0400

    Moved providers so users don't hav eto use forRoot if they don't want.

--- samples/client/petstore/typescript-angular-v4.3/npm/api.module.ts
+++ samples/client/petstore/typescript-angular-v4.3/npm/api.module.ts
@@ -10,18 +10,17 @@ import { UserService } from './api/user.service';
 @NgModule({
   imports:      [ CommonModule, HttpClientModule ],
   declarations: [],
-  exports:      []
+  exports:      [],
+  providers: [ 
+    PetService,
+    StoreService,
+    UserService ]
 })
 export class ApiModule {
     public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
         return {
             ngModule: ApiModule,
-            providers: [ 
-            { provide: Configuration, useFactory: configurationFactory}, 
-            PetService,
-            StoreService,
-            UserService
-            ]
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
         }
     }
 
