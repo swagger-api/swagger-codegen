e07f24b85d2bd0b57bae8d17db49bced4ee1b255
Gabriele Bonetti <gabriele.bonetti@gmail.com>
2017-11-11 22:41:40 +0100

    use wb mode in file deserialization

--- samples/client/petstore-security-test/python/petstore_api/api_client.py
+++ samples/client/petstore-security-test/python/petstore_api/api_client.py
@@ -528,7 +528,7 @@ class ApiClient(object):
                 group(1)
             path = os.path.join(os.path.dirname(path), filename)
 
-        with open(path, "w") as f:
+        with open(path, "wb") as f:
             f.write(response.data)
 
         return path
