fd0295e11c96adfb1f62723340eb869164833faa
Mehdi Jouan <memedidou@gmail.com>
2017-12-26 09:40:36 +0100

    fix import issue after running shell script

--- samples/client/petstore/spring-cloud/src/main/java/io/swagger/api/StoreApi.java
+++ samples/client/petstore/spring-cloud/src/main/java/io/swagger/api/StoreApi.java
@@ -6,16 +6,22 @@
 package io.swagger.api;
 
 import java.util.Map;
-
-import javax.validation.Valid;
-
+import io.swagger.model.Order;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
-import io.swagger.model.Order;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
 
 @Api(value = "Store", description = "the Store API")
 public interface StoreApi {
