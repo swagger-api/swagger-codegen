4cd137c577cce2e664166a7f96492aba7a56efa9
Eric Hyche <ehyche@groupon.com>
2018-01-02 16:02:09 -0500

    Change models from class to struct.
    This fixes issue https://github.com/swagger-api/swagger-codegen/issues/6941.
    
    In this change, we make our Swift4 generated model objects struct instead of class. However, in order to do this, we needed to handle the following edge cases:
    
    * Inheritance and polymorphism (allOf)
      * With classes, we use inheritance. So therefore, the parent properties are ONLY on the parent generated class, and the model object which derives from the parent class picks up those properties through inheritance.
      * However, structs do not support inheritance. So we simply duplicate the parent allOf properties in the child struct.
    * We have to handle the case where the property name on the struct may be different than the property name in the JSON. By default, the Codable protocol assumes that the JSON property name is the same as the struct property name. If they need to be different, then we generate a CodingKeys string enum, which contains the mapping between struct property name and JSON property name.
    * additionalProperties. We cannot use the default Codable implementation for the additionalProperties, since it will look for an actual dictionary called "additionalProperties" in the JSON. Therefore, for model objects which have additionalProperties, we must generate our own implementation for the Decodable and Encodable protocols.
    
    I have run ./bin/swift4-all.sh and ./bin/swift4-test.sh to re-generate all of the sources, and I have verified that the generated code in samples/clients/test/swift4/default builds and the unit tests pass.

--- samples/client/petstore/swift4/objcCompatible/PetstoreClient/Classes/Swaggers/Models/Order.swift
+++ samples/client/petstore/swift4/objcCompatible/PetstoreClient/Classes/Swaggers/Models/Order.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 
-open class Order: Codable {
+public struct Order: Codable {
 
     public enum Status: String, Codable { 
         case placed = "placed"
@@ -45,42 +45,6 @@ open class Order: Codable {
     }
 
 
-    
-    public init(id: Int64?, petId: Int64?, quantity: Int?, shipDate: Date?, status: Status?, complete: Bool?) {
-        self.id = id
-        self.petId = petId
-        self.quantity = quantity
-        self.shipDate = shipDate
-        self.status = status
-        self.complete = complete
-    }
-    
 
-    // Encodable protocol methods
-
-    public func encode(to encoder: Encoder) throws {
-
-        var container = encoder.container(keyedBy: String.self)
-
-        try container.encodeIfPresent(id, forKey: "id")
-        try container.encodeIfPresent(petId, forKey: "petId")
-        try container.encodeIfPresent(quantity, forKey: "quantity")
-        try container.encodeIfPresent(shipDate, forKey: "shipDate")
-        try container.encodeIfPresent(status, forKey: "status")
-        try container.encodeIfPresent(complete, forKey: "complete")
-    }
-
-    // Decodable protocol methods
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: String.self)
-
-        id = try container.decodeIfPresent(Int64.self, forKey: "id")
-        petId = try container.decodeIfPresent(Int64.self, forKey: "petId")
-        quantity = try container.decodeIfPresent(Int.self, forKey: "quantity")
-        shipDate = try container.decodeIfPresent(Date.self, forKey: "shipDate")
-        status = try container.decodeIfPresent(Status.self, forKey: "status")
-        complete = try container.decodeIfPresent(Bool.self, forKey: "complete")
-    }
 }
 
