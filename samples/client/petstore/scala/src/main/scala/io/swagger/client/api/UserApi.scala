f240ea3e5aa503a2e211f4d5a1a63d9337d8cf17
Jim Schubert <james.schubert@gmail.com>
2018-01-03 22:17:29 -0500

    [scala] Regenerate client sample

--- samples/client/petstore/scala/src/main/scala/io/swagger/client/api/UserApi.scala
+++ samples/client/petstore/scala/src/main/scala/io/swagger/client/api/UserApi.scala
@@ -24,6 +24,7 @@ import javax.ws.rs.core.MediaType
 
 import java.io.File
 import java.util.Date
+import java.util.TimeZone
 
 import scala.collection.mutable.HashMap
 
@@ -41,15 +42,24 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
 
-import org.json4s._
-
 class UserApi(
   val defBasePath: String = "http://petstore.swagger.io/v2",
   defApiInvoker: ApiInvoker = ApiInvoker
 ) {
+  import org.json4s._
 
+  private lazy val dateTimeFormatter = {
+    val formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    formatter.setTimeZone(TimeZone.getTimeZone("UTC"))
+    formatter
+  }
+  private val dateFormatter = {
+    val formatter = new SimpleDateFormat("yyyy-MM-dd")
+    formatter.setTimeZone(TimeZone.getTimeZone("UTC"))
+    formatter
+  }
   implicit val formats = new org.json4s.DefaultFormats {
-    override def dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS+0000")
+    override def dateFormatter = dateTimeFormatter
   }
   implicit val stringReader: ClientResponseReader[String] = ClientResponseReaders.StringReader
   implicit val unitReader: ClientResponseReader[Unit] = ClientResponseReaders.UnitReader
