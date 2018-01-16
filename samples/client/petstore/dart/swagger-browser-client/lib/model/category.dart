9f23d48645dd70199ec1520a9d81bd7c4715cfc9
Jörn Ahrens <joern.ahrens@jimdo.com>
2018-01-16 13:35:13 +0100

    swagger-browser-client for browserClient testcases

---
+++ samples/client/petstore/dart/swagger-browser-client/lib/model/category.dart
@@ -0,0 +1,44 @@
+part of swagger.api;
+
+class Category {
+  
+  int id = null;
+  
+
+  String name = null;
+  
+  Category();
+
+  @override
+  String toString() {
+    return 'Category[id=$id, name=$name, ]';
+  }
+
+  Category.fromJson(Map<String, dynamic> json) {
+    if (json == null) return;
+    id = json['id'];
+    name = json['name'];
+  }
+
+  static List<Category> listFromJson(List<Map<String, dynamic>> json) {
+    var list = new List<Category>();
+    if (json != null && json.length > 0) {
+      json.forEach((Map<String, dynamic> value) => list.add(new Category.fromJson(value)));
+    }
+    return list;
+  }
+
+  static List<Map<String, dynamic>> toMapList(List<Category> list) {
+    var listResult = new List<Map<String, dynamic>>();
+    list.forEach((Category it) => listResult.add(it.toMap()));
+    return listResult;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name
+     };
+  }
+}
+
---
+++ samples/client/petstore/dart/swagger-browser-client/lib/model/tag.dart
@@ -0,0 +1,44 @@
+part of swagger.api;
+
+class Tag {
+  
+  int id = null;
+  
+
+  String name = null;
+  
+  Tag();
+
+  @override
+  String toString() {
+    return 'Tag[id=$id, name=$name, ]';
+  }
+
+  Tag.fromJson(Map<String, dynamic> json) {
+    if (json == null) return;
+    id = json['id'];
+    name = json['name'];
+  }
+
+  static List<Tag> listFromJson(List<Map<String, dynamic>> json) {
+    var list = new List<Tag>();
+    if (json != null && json.length > 0) {
+      json.forEach((Map<String, dynamic> value) => list.add(new Tag.fromJson(value)));
+    }
+    return list;
+  }
+
+  static List<Map<String, dynamic>> toMapList(List<Tag> list) {
+    var listResult = new List<Map<String, dynamic>>();
+    list.forEach((Tag it) => listResult.add(it.toMap()));
+    return listResult;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name
+     };
+  }
+}
+
