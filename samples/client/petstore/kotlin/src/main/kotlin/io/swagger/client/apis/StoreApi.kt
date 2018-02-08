ad5d8793be47d92c67cb1d289ecec4382b4e1501
Ken Gilmer <kggilmer@amazon.com>
2018-02-08 12:11:29 -0800

    Update petstore client generated code per PR checklist.

--- samples/client/petstore/kotlin/src/main/kotlin/io/swagger/client/apis/StoreApi.kt
+++ samples/client/petstore/kotlin/src/main/kotlin/io/swagger/client/apis/StoreApi.kt
@@ -14,8 +14,9 @@ package io.swagger.client.apis
 import io.swagger.client.models.Order
 
 import io.swagger.client.infrastructure.*
+import okhttp3.OkHttpClient
 
-class StoreApi(basePath: kotlin.String = "http://petstore.swagger.io/v2") : ApiClient(basePath) {
+class StoreApi(basePath: kotlin.String = "http://petstore.swagger.io/v2", httpClient: OkHttpClient = OkHttpClient()) : ApiClient(basePath, httpClient) {
 
     /**
     * Delete purchase order by ID
