a06569fe6104247215333fb99ab52bc55f83f360
Ignacio Molina Cuquerella <imolina@paradigmadigital.com>
2018-02-21 17:37:33 +0100

    Add missing templates and generate samples

--- samples/server/petstore/java-play-framework-async/app/controllers/StoreApiControllerImp.java
+++ samples/server/petstore/java-play-framework-async/app/controllers/StoreApiControllerImp.java
@@ -15,23 +15,25 @@ public class StoreApiControllerImp implements StoreApiControllerImpInterface {
     public void deleteOrder(String orderId) throws Exception {
         //Do your magic!!!
     }
-
     @Override
-    public Map<String, Integer> getInventory() throws Exception {
+    public CompletionStage<Map<String, Integer>> getInventory() throws Exception {
         //Do your magic!!!
-        return new HashMap<String, Integer>();
+        return CompletableFuture.supplyAsync(() -> { 
+            return new HashMap<String, Integer>();
+        });
     }
-
     @Override
-    public Order getOrderById( @Min(1) @Max(5)Long orderId) throws Exception {
+    public CompletionStage<Order> getOrderById( @Min(1) @Max(5)Long orderId) throws Exception {
         //Do your magic!!!
-        return new Order();
+        return CompletableFuture.supplyAsync(() -> { 
+            return new Order();
+        });
     }
-
     @Override
-    public Order placeOrder(Order body) throws Exception {
+    public CompletionStage<Order> placeOrder(Order body) throws Exception {
         //Do your magic!!!
-        return new Order();
+        return CompletableFuture.supplyAsync(() -> { 
+            return new Order();
+        });
     }
-
 }
