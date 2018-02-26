3992702a2f815a0bf853956b7de7038a1dff28e6
tcslater <tim.slater@smartward.com.au>
2018-02-26 11:15:38 +1100

    Revert "Moving kotlin support code to swagger-codegen-generators"
    This reverts commit ee834db614a7f0cf8ce79c0714e57ee2f4d16e7f.
    
    Revert test removal

---
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/options/KotlinClientCodegenOptionsProvider.java
@@ -0,0 +1,40 @@
+package io.swagger.codegen.options;
+
+import io.swagger.codegen.CodegenConstants;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public class KotlinClientCodegenOptionsProvider implements OptionsProvider {
+    public static final String PACKAGE_NAME_VALUE = "io.swagger.tests.kotlin";
+    public static final String ARTIFACT_VERSION_VALUE = "0.0.1-SNAPSHOT";
+    public static final String ARTIFACT_ID = "swagger-kotlin-test";
+    public static final String GROUP_ID = "io.swagger.tests";
+    public static final String SOURCE_FOLDER = "./generated/kotlin";
+    public static final String ENUM_PROPERTY_NAMING = "camelCase";
+
+    @Override
+    public String getLanguage() {
+        return "kotlin";
+    }
+
+    @Override
+    public Map<String, String> createOptions() {
+        ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<String, String>();
+        return builder
+                .put(CodegenConstants.PACKAGE_NAME, PACKAGE_NAME_VALUE)
+                .put(CodegenConstants.ARTIFACT_VERSION, ARTIFACT_VERSION_VALUE)
+                .put(CodegenConstants.ARTIFACT_ID, ARTIFACT_ID)
+                .put(CodegenConstants.GROUP_ID, GROUP_ID)
+                .put(CodegenConstants.SOURCE_FOLDER, SOURCE_FOLDER)
+                .put(CodegenConstants.ENUM_PROPERTY_NAMING, ENUM_PROPERTY_NAMING)
+                .build();
+    }
+
+    @Override
+    public boolean isServer() {
+        return false;
+    }
+}
+
