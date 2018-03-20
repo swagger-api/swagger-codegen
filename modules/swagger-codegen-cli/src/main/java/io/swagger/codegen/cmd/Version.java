df246ebedee75a0ddad5e244363b636e7cd55d56
Jeremie Bresson <dev@jmini.fr>
2018-03-20 17:30:34 +0100

    Modify 'meta' in order to work with v3

--- modules/swagger-codegen-cli/src/main/java/io/swagger/codegen/cmd/Version.java
+++ modules/swagger-codegen-cli/src/main/java/io/swagger/codegen/cmd/Version.java
@@ -11,6 +11,9 @@ public class Version implements Runnable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Meta.class);
 
+    public static final String SWAGGER_CODEGEN_VERSION = "swagger-codegen.version";
+    public static final String SWAGGER_CODEGEN_GENERATORS_VERSION = "swagger-codegen.generators.version";
+
     private static final String VERSION_PLACEHOLDER = "${project.version}";
 
     private static final String UNREADABLE_VERSION = "unreadable";
@@ -18,6 +21,10 @@ public class Version implements Runnable {
     private static final String UNKNOWN_VERSION = "unknown";
 
     public static String readVersionFromResources() {
+        return readVersionFromResources(SWAGGER_CODEGEN_VERSION);
+    }
+
+    public static String readVersionFromResources(String key) {
         Properties versionProperties = new Properties();
         try (InputStream is = Version.class.getResourceAsStream("/version.properties")) {
             versionProperties.load(is);
@@ -26,7 +33,7 @@ public class Version implements Runnable {
             return UNREADABLE_VERSION;
         }
 
-        String version = versionProperties.getProperty("version", UNKNOWN_VERSION).trim();
+        String version = versionProperties.getProperty(key, UNKNOWN_VERSION).trim();
         if (VERSION_PLACEHOLDER.equals(version)) {
             return UNSET_VERSION;
         } else {
