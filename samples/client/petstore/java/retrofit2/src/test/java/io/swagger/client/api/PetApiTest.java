234d8090ecd05c10278c1bb2087d05e3e4e4c3e6
Ignacio Molina Cuquerella <imolina@paradigmadigital.com>
2018-04-27 12:27:32 +0200

    fix failing test in samples

--- samples/client/petstore/java/retrofit2/src/test/java/io/swagger/client/api/PetApiTest.java
+++ samples/client/petstore/java/retrofit2/src/test/java/io/swagger/client/api/PetApiTest.java
@@ -12,16 +12,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.*;
-
-import retrofit2.Response;
-
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.MultipartBody;
-
-import static org.junit.Assert.*;
-
 /**
  * API tests for PetApi
  */
@@ -120,12 +110,6 @@ public class PetApiTest {
         // api.updatePetWithForm(petId, name, status);
 
         // TODO: test validations
-        File file = new File("hello.txt");
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        writer.write("Hello world!");
-        writer.close();        
-        final RequestBody body = RequestBody.create(MediaType.parse("text/plain"), file);
-        api.uploadFile(pet.getId(), null, MultipartBody.Part.createFormData("datafile", file.getName(), body)).execute();
     }
     /**
      * uploads an image
