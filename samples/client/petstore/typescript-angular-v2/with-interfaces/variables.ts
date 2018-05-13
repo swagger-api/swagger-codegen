3574599d88389839e8c9bfe961595bf1f1288ef2
Chris Mellard <chrism@datacom.co.nz>
2018-05-13 21:36:40 +1200

    Issue7949 - Fixing up rxjs Observable import that was failing under latest rxjs package/angular6

--- samples/client/petstore/typescript-angular-v2/with-interfaces/variables.ts
+++ samples/client/petstore/typescript-angular-v2/with-interfaces/variables.ts
@@ -1,5 +1,5 @@
 import { OpaqueToken } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 
 export const BASE_PATH = new OpaqueToken('basePath');
 export const BASE_PATH_OBSERVABLE = new OpaqueToken('basePathObservable');
--- samples/client/petstore/typescript-angular-v5/npm/variables.ts
+++ samples/client/petstore/typescript-angular-v5/npm/variables.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 
 export const BASE_PATH = new InjectionToken<string>('basePath');
 export const BASE_PATH_OBSERVABLE = new InjectionToken<Observable<string>>('basePathObservable');
