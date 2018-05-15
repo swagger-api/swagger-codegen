34d35564d640cefcc693a667ca48ffbb4d055db2
Benjamin Gill <benjamin.gill@metaswitch.com>
2018-05-15 11:27:46 +0100

    Add Rust-server-specific spec
    Adds endpoints to test XML support. Removes inline additional
    properties, which we do not yet support

--- bin/rust-server-petstore.sh
+++ bin/rust-server-petstore.sh
@@ -26,6 +26,10 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/rust-server -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l rust-server -o samples/server/petstore/rust-server -DpackageName=petstore_api"
+ags="$@ generate \
+  -t modules/swagger-codegen/src/main/resources/rust-server \
+  -i modules/swagger-codegen/src/test/resources/2_0/rust-server/petstore-with-fake-endpoints-models-for-testing.yaml \
+  -l rust-server -o samples/server/petstore/rust-server \
+  -DpackageName=petstore_api"
 
 java $JAVA_OPTS -jar $executable $ags
