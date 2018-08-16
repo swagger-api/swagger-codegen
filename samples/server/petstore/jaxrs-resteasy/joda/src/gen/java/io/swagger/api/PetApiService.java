85bbf694f49c6d64e17820dbb6d163d097ef0cf5
Hugo Mercado <hugo.mario.mercado@gmail.com>
2018-08-16 03:38:44 -0500

    updated resteasy joda sample.

--- samples/server/petstore/jaxrs-resteasy/joda/src/gen/java/io/swagger/api/PetApiService.java
+++ samples/server/petstore/jaxrs-resteasy/joda/src/gen/java/io/swagger/api/PetApiService.java
@@ -2,11 +2,11 @@ package io.swagger.api;
 
 import io.swagger.api.*;
 import io.swagger.model.*;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
-
+import java.io.File;
 import io.swagger.model.ModelApiResponse;
 import io.swagger.model.Pet;
-
 
 import java.util.List;
 import io.swagger.api.NotFoundException;
@@ -16,33 +16,13 @@ import java.io.InputStream;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-
-
 public interface PetApiService {
-  
-      Response addPet(Pet pet,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response deletePet(Integer petId,String apiKey,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response findPetsByStatus(List<String> status,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response findPetsByTags(List<String> tags,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response getPetById(Integer petId,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response updatePet(Pet pet,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response updatePetWithForm(Integer petId,Object body,SecurityContext securityContext)
-      throws NotFoundException;
-  
-      Response uploadFile(Integer petId,Object body,SecurityContext securityContext)
-      throws NotFoundException;
-  
+      Response addPet(Pet body,SecurityContext securityContext) throws NotFoundException;
+      Response deletePet(Integer petId,String apiKey,SecurityContext securityContext) throws NotFoundException;
+      Response findPetsByStatus(List<String> status,SecurityContext securityContext) throws NotFoundException;
+      Response findPetsByTags(List<String> tags,SecurityContext securityContext) throws NotFoundException;
+      Response getPetById(Integer petId,SecurityContext securityContext) throws NotFoundException;
+      Response updatePet(Pet body,SecurityContext securityContext) throws NotFoundException;
+      Response updatePetWithForm(Integer petId,String name,String status,SecurityContext securityContext) throws NotFoundException;
+      Response uploadFile(MultipartFormDataInput input,Integer petId,SecurityContext securityContext) throws NotFoundException;
 }
-
--- samples/server/petstore/jaxrs-resteasy/joda/src/main/java/io/swagger/api/impl/StoreApiServiceImpl.java
+++ samples/server/petstore/jaxrs-resteasy/joda/src/main/java/io/swagger/api/impl/StoreApiServiceImpl.java
@@ -3,10 +3,8 @@ package io.swagger.api.impl;
 import io.swagger.api.*;
 import io.swagger.model.*;
 
-
 import java.util.Map;
 import io.swagger.model.Order;
-
 
 import java.util.List;
 import io.swagger.api.NotFoundException;
@@ -18,33 +16,21 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 @RequestScoped
-
-
 public class StoreApiServiceImpl implements StoreApiService {
-  
-      public Response deleteOrder(String orderId,SecurityContext securityContext)
-      throws NotFoundException {
+      public Response deleteOrder(String orderId,SecurityContext securityContext) throws NotFoundException {
       // do some magic!
       return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
   }
-  
-      public Response getInventory(SecurityContext securityContext)
-      throws NotFoundException {
+      public Response getInventory(SecurityContext securityContext) throws NotFoundException {
       // do some magic!
       return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
   }
-  
-      public Response getOrderById(Integer orderId,SecurityContext securityContext)
-      throws NotFoundException {
+      public Response getOrderById(Integer orderId,SecurityContext securityContext) throws NotFoundException {
       // do some magic!
       return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
   }
-  
-      public Response placeOrder(Order order,SecurityContext securityContext)
-      throws NotFoundException {
+      public Response placeOrder(Order body,SecurityContext securityContext) throws NotFoundException {
       // do some magic!
       return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
   }
-  
 }
-
