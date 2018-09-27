68dfc28dbcf14cfa1ce7d2eddeca6b3810de772d
Hugo Mercado <hugo.mario.mercado@gmail.com>
2018-09-27 07:16:16 -0500

    updated springboot petstore server beanvalidation sample

---
+++ samples/server/petstore/springboot-beanvalidation/src/test/java/io/swagger/api/StoreApiControllerIntegrationTest.java
@@ -0,0 +1,52 @@
+package io.swagger.api;
+
+import java.util.Map;
+import io.swagger.model.Order;
+
+import java.util.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreApiControllerIntegrationTest {
+
+    @Autowired
+    private StoreApi api;
+
+    @Test
+    public void deleteOrderTest() throws Exception {
+        String orderId = "orderId_example";
+        ResponseEntity<Void> responseEntity = api.deleteOrder(orderId);
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void getInventoryTest() throws Exception {
+        ResponseEntity<Map<String, Integer>> responseEntity = api.getInventory();
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void getOrderByIdTest() throws Exception {
+        Integer orderId = 56;
+        ResponseEntity<Order> responseEntity = api.getOrderById(orderId);
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void placeOrderTest() throws Exception {
+        Order body = new Order();
+        ResponseEntity<Order> responseEntity = api.placeOrder(body);
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
+    }
+
+}
