d9ab1049cc6c1b6d12a52b02118862f032305fac
Rafael López <ralphdoe12@gmail.com>
2018-10-23 23:18:08 -0500

    samples updates - samples/client/petstore/typescript-angular-v4.3/with-interfaces/

--- samples/client/petstore/typescript-angular-v4.3/with-interfaces/api/pet.serviceInterface.ts
+++ samples/client/petstore/typescript-angular-v4.3/with-interfaces/api/pet.serviceInterface.ts
@@ -13,7 +13,6 @@ import { HttpHeaders }                                       from '@angular/comm
 
 import { Observable }                                        from 'rxjs/Observable';
 
-
 import { ApiResponse } from '../model/apiResponse';
 import { Pet } from '../model/pet';
 
