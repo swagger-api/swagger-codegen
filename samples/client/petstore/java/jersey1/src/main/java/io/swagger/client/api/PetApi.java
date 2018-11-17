b0c34f89d9f632d9af696abb82778f631ff544fd
Hugo Mercado <hugo.mario.mercado@gmail.com>
2018-11-17 12:42:34 -0500

    updated jersey1 samples.

--- samples/client/petstore/java/jersey1/src/main/java/io/swagger/client/api/PetApi.java
+++ samples/client/petstore/java/jersey1/src/main/java/io/swagger/client/api/PetApi.java
@@ -26,7 +26,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 public class PetApi {
   private ApiClient apiClient;
 
--- samples/client/petstore/java/jersey1/src/main/java/io/swagger/client/api/StoreApi.java
+++ samples/client/petstore/java/jersey1/src/main/java/io/swagger/client/api/StoreApi.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 public class StoreApi {
   private ApiClient apiClient;
 
