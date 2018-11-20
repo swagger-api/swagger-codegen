73d6ed9bd98bd3a9faf2fd454e34266c0723cfc0
frantuma <frantuma@yahoo.com>
2018-11-20 13:36:25 +0100

    update spring samples

--- bin/spring-mvc-petstore-server.sh
+++ bin/spring-mvc-petstore-server.sh
@@ -29,6 +29,6 @@ export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/
 ags="$@ generate -t modules/swagger-codegen/src/main/resources/JavaSpring -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l spring -c bin/spring-mvc-petstore-server.json -o samples/server/petstore/spring-mvc -DhideGenerationTimestamp=true"
 
 rm -rf samples/server/petstore/spring-mvc/src/main
-rm -rf samples/server/petstore/spring-mvc-j8-localdatetime/src/test/java/io
+rm -rf samples/server/petstore/spring-mvc/src/test/java/io
 find samples/server/petstore/spring-mvc -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
 java $JAVA_OPTS -jar $executable $ags
