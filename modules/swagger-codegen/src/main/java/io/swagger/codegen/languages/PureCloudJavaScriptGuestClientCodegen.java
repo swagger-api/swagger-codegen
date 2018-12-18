dd650f42b7a5f20fc44f13743fe03f951bd83dff
Tim Smith <tim.j.smith@genesys.com>
2018-12-18 13:30:33 -0700

    API-3534 js guest codegen

---
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PureCloudJavaScriptGuestClientCodegen.java
@@ -0,0 +1,6 @@
+package io.swagger.codegen.languages;
+
+public class PureCloudJavaScriptGuestClientCodegen extends PureCloudJavaScriptClientCodegen {
+    @Override
+    public String getName() { return "purecloudjavascript-guest"; }
+}
