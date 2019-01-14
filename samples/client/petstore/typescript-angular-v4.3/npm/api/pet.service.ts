f9cd5268ad7bd8fbcafffe9606eea3f971f27451
m.buils <m.buils@hager.fr>
2019-01-14 17:38:14 +0100

    [typescript-angular] error TS1345: An expression of type 'void' cannot be tested for truthiness #8836

--- samples/client/petstore/typescript-angular-v4.3/npm/api/pet.service.ts
+++ samples/client/petstore/typescript-angular-v4.3/npm/api/pet.service.ts
@@ -432,7 +432,7 @@ export class PetService {
 
         const canConsumeForm = this.canConsumeForm(consumes);
 
-        let formParams: { append(param: string, value: any): void; };
+        let formParams: { append(param: string, value: any): void | HttpParams; };
         let useForm = false;
         let convertFormParamsToString = false;
         if (useForm) {
@@ -505,7 +505,7 @@ export class PetService {
 
         const canConsumeForm = this.canConsumeForm(consumes);
 
-        let formParams: { append(param: string, value: any): void; };
+        let formParams: { append(param: string, value: any): void | HttpParams; };
         let useForm = false;
         let convertFormParamsToString = false;
         // use FormData to transmit files using content-type "multipart/form-data"
