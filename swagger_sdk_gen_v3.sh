ec9b41893a780bbb69812ca9ac4411cb3adb0e7b
Ankit <ankit.verma@capillarytech.com>
2019-08-26 20:38:40 +0530

    sdk-v3

--- swagger_sdk_gen_v3.sh
+++ swagger_sdk_gen_v3.sh
@@ -24,7 +24,7 @@ then
   -c config.json
   tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
   mvn3 clean deploy -f intouch_api/java_client/java/pom.xml
-  fpm -f -s "dir" -t "deb" -a "all" -n "java-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/java_client --deb-no-default-config-files  java="/usr/share/java/capillary-libs/swagger-sdk"
+  fpm -f -s "dir" -t "deb" -a "all" -n "java-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/java_client --deb-no-default-config-files  java="/usr/share/java/capillary-libs/swagger-v3-sdk"
 elif [ "$Client" = "c#" ]
 then rm -rf intouch_api/csharp_client/c#
    java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
@@ -33,7 +33,7 @@ then rm -rf intouch_api/csharp_client/c#
   -DtargetFramework=v$Version \
   -o intouch_api/csharp_client/c#
   tar cvzf intouch_api/csharp_client/c#swagger_sdK_$BUILD_NUMBER.tar.gz -C ./intouch_api/csharp_client/c#/ .
-  fpm -f -s "dir" -t "deb" -a "all" -n "c#-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/csharp_client --deb-no-default-config-files  csharp="/usr/share/c#/capillary-libs/swagger-sdk"
+  fpm -f -s "dir" -t "deb" -a "all" -n "c#-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/csharp_client --deb-no-default-config-files  csharp="/usr/share/c#/capillary-libs/swagger-v3-sdk"
 elif [ "$Client" = "php" ]
 then rm -rf intouch_api/php_client/php
    java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
@@ -41,7 +41,7 @@ then rm -rf intouch_api/php_client/php
   -l php \
   -o intouch_api/php_client/php
   tar cvzf intouch_api/php_client/php_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/php_client/php/ .
-  fpm -f -s "dir" -t "deb" -a "all" -n "swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/php_client --deb-no-default-config-files  php="/usr/share/php/capillary-libs/swagger-sdk"
+  fpm -f -s "dir" -t "deb" -a "all" -n "swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/php_client --deb-no-default-config-files  php="/usr/share/php/capillary-libs/swagger-v3-sdk"
 elif [ "$Client" = "nodejs" ]
 then rm -rf intouch_api/nodejs_client
 	mkdir -p intouch_api/nodejs_client/
@@ -49,7 +49,7 @@ then rm -rf intouch_api/nodejs_client
      npm install swagger-js-codegen
      cd swagger-js-codegen
      node ../nodejs_sdk_gen > ../intouch_api/nodejs_client/node_$BUILD_NUMBER
-     fpm -f -s "dir" -t "deb" -a "all" -n "node-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/nodejs_client --deb-no-default-config-files  nodejs="/usr/share/nodejs/capillary-libs/swagger-sdk"
+     fpm -f -s "dir" -t "deb" -a "all" -n "node-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/nodejs_client --deb-no-default-config-files  nodejs="/usr/share/nodejs/capillary-libs/swagger-v3-sdk"
 elif [ "$Client" = "python" ]
 then rm -rf intouch_api/python_client
   java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
@@ -57,7 +57,7 @@ then rm -rf intouch_api/python_client
   -l python \
   -o intouch_api/python_client/python
   tar cvzf intouch_api/python_client/python_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/python_client/python/ .
-  fpm -f -s "dir" -t "deb" -a "all" -n "py-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/python_client --deb-no-default-config-files  python="/usr/share/python/capillary-libs/swagger-sdk"
+  fpm -f -s "dir" -t "deb" -a "all" -n "py-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/python_client --deb-no-default-config-files  python="/usr/share/python/capillary-libs/swagger-v3-sdk"
 else " no client is selected"
 fi
 echo "SWAGGER SDK SUCCESSFULLY GENERATED"
