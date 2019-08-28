f1602c1ba5d7e279f7f0b4000c6c1041e4223808
Ankit <ankit.verma@capillarytech.com>
2019-08-28 18:54:50 +0530

    re

--- swagger_sdk_gen_v3.sh
+++ swagger_sdk_gen_v3.sh
@@ -21,15 +21,11 @@ then
   -l java \
   -DdateLibrary=java8 \
   -o intouch_api/java_client/java \
-  -c config.json \  
-  --group-id com.capillary.platforms \ 
-  --artifact-version $version \
-  --model-package com.capillary.platforms.intouch-api.models.v3 \ 
-  --api-package com.capillary.platforms.intouch-api.api.v3 \
-  --artifact-id intouch-api-client.v3
+  -c config.json
   tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
   mvn3 clean deploy -f intouch_api/java_client/java/pom.xml
   fpm -f -s "dir" -t "deb" -a "all" -n "java-swagger-v3-sdk" -v $BUILD_NUMBER -C ./intouch_api/java_client --deb-no-default-config-files  java="/usr/share/java/capillary-libs/swagger-v3-sdk"
+
 elif [ "$Client" = "c#" ]
 then rm -rf intouch_api/csharp_client/c#
    java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
