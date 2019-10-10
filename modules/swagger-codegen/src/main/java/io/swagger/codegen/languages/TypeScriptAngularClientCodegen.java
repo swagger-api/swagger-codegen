28f6f97cbe59cfa2e5cf093b3133feffd59d44cf
frantuma <frantuma@yahoo.com>
2019-10-10 16:37:15 +0200

    refs #7866 - revert #9700

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngularClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngularClientCodegen.java
@@ -390,7 +390,7 @@ public class TypeScriptAngularClientCodegen extends AbstractTypeScriptClientCode
             if (!im.equals(cm.classname)) {
                 HashMap<String, String> tsImport = new HashMap<>();
                 tsImport.put("classname", im);
-                tsImport.put("filename", im);
+                tsImport.put("filename", toModelFilename(im));
                 tsImports.add(tsImport);
             }
         }
@@ -425,7 +425,7 @@ public class TypeScriptAngularClientCodegen extends AbstractTypeScriptClientCode
 
     @Override
     public String toModelImport(String name) {
-        return modelPackage() + "/" + name;
+        return modelPackage() + "/" + toModelFilename(name);
     }
 
     public String getNpmName() {
