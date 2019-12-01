ea52d0bff7b79ca908719e28b325e70bfae617de
HugoMario <hugo.mario.mercado@gmail.com>
2019-12-01 00:25:08 -0500

    use default (keyword) on interfaces when java8 option is used.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SpringCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SpringCodegen.java
@@ -128,6 +128,8 @@ public class SpringCodegen extends AbstractJavaCodegen
             if (!additionalProperties.containsKey(DATE_LIBRARY)) {
                 setDateLibrary("java8");
             }
+        } else {
+            defaultInterfaces = false;
         }
 
         // set invokerPackage as basePackage
