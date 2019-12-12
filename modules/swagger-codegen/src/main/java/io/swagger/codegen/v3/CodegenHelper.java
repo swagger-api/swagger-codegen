39d84b2a827bc7244091e9a2b3078ad59dad3a87
Ashish <sinha8ashish@gmail.com>
2019-12-12 15:44:09 +1100

    Update CodegenHelper.java
    Added few more speacial characters ==, &&, ||

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/v3/CodegenHelper.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/v3/CodegenHelper.java
@@ -107,5 +107,9 @@ public class CodegenHelper {
         specialCharReplacements.put("<=", "Less_Than_Or_Equal_To");
         specialCharReplacements.put(">=", "Greater_Than_Or_Equal_To");
         specialCharReplacements.put("!=", "Not_Equal");
+		
+		specialCharReplacements.put("==", "Is_Equal_To");
+		specialCharReplacements.put("&&", "And");
+		specialCharReplacements.put("||", "Or");
     }
 }
