fbac41344c71b7f23fc80bba84c9a1e90a759e05
mohitsarewar1481 <mohit.sarewar@capillarytech.com>
2020-01-27 18:10:02 +0530

    add swagger gen file for fileservice

---
+++ swagger_sdk_gen_fileService.sh
@@ -0,0 +1,30 @@
+if $Build_Codegen ; then
+    mvn3 clean package -Dmaven.test.skip=true
+fi
+if [ "$Branch" = "master" ]
+  then
+    url="http://fileservice.capillary.in/tl-docs-test/v2/api-docs"
+    version="https://fileservice.capillary.in/v1/meta/version"
+elif [ "$Branch" = "production" ]
+  then
+    url="http://fileservice.capillary.in/tl-docs-test/v2/api-docs"
+    version="https://fileservice.capillary.in/v1/meta/version"
+else " No Branch is selected"
+fi
+curl -k $version -o config.json
+echo "GENERATING SDK"
+if [ "$Client" = "java" ]
+then 
+  rm -rf fileservice/java_client/java
+  java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+  -i $url  \
+  -l java \
+  -DdateLibrary=java8 \
+  -o fileservice/java_client/java \
+  -c config.json
+  tar cvzf fileservice/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
+  mvn3 clean deploy -f fileservice/java_client/java/pom.xml
+  fpm -f -s "dir" -t "deb" -a "all" -n "swagger_sdk_gen_fileService" -v $BUILD_NUMBER -C ./fileservice/java_client --deb-no-default-config-files  java="/usr/share/java/capillary-libs/swagger_sdk_gen_fileService"
+else " no client is selected"
+fi
+echo "SWAGGER SDK SUCCESSFULLY GENERATED"
