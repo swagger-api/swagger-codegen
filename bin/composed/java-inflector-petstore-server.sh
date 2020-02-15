92c65da56916ce786346c5272f2694c761d21d34
HugoMario <hugo.mario.mercado@gmail.com>
2020-02-15 18:47:39 -0500

    created a new list of bin scripts

---
+++ bin/composed/java-inflector-petstore-server.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+
+if [ ! -f "$executable" ]
+then
+  mvn clean package
+fi
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
+ags="$@ generate -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-fake-endpoints-models-for-testing.yaml -l inflector -o samples/server/petstore/java-inflector -DhideGenerationTimestamp=true"
+
+echo "Removing files and folders under samples/server/petstore/java-inflector/src/main"
+rm -rf samples/server/petstore/java-inflector/src/main
+rm -rf samples/server/petstore/java-inflector/src/gen
+find samples/server/petstore/java-inflector -maxdepth 1 -type f ! -name "README.md" -exec rm {} +
+
+java $JAVA_OPTS -jar $executable $ags
