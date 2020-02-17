2851d8776fcd696c27f60a6d051b7b7e4b3bc1c1
HugoMario <hugo.mario.mercado@gmail.com>
2020-02-16 23:14:15 -0500

    added java feign sample

---
+++ samples/composed/client/petstore/java/feign/build.sbt
@@ -0,0 +1,26 @@
+lazy val root = (project in file(".")).
+  settings(
+    organization := "io.swagger",
+    name := "swagger-petstore-feign",
+    version := "1.0.0",
+    scalaVersion := "2.11.4",
+    scalacOptions ++= Seq("-feature"),
+    javacOptions in compile ++= Seq("-Xlint:deprecation"),
+    publishArtifact in (Compile, packageDoc) := false,
+    resolvers += Resolver.mavenLocal,
+    libraryDependencies ++= Seq(
+      "io.swagger.core.v3" % "swagger-annotations" % "2.0.0" % "compile",
+      "com.netflix.feign" % "feign-core" % "9.4.0" % "compile",
+      "com.netflix.feign" % "feign-jackson" % "9.4.0" % "compile",
+      "com.netflix.feign" % "feign-slf4j" % "9.4.0" % "compile",
+      "io.github.openfeign.form" % "feign-form" % "2.1.0" % "compile",
+      "com.fasterxml.jackson.core" % "jackson-core" % "2.10.1" % "compile",
+      "com.fasterxml.jackson.core" % "jackson-annotations" % "2.10.1" % "compile",
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.10.1" % "compile",
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.10.1" % "compile",
+      "org.apache.oltu.oauth2" % "org.apache.oltu.oauth2.client" % "1.0.2" % "compile",
+      "com.brsanthu" % "migbase64" % "2.2" % "compile",
+      "junit" % "junit" % "4.12" % "test",
+      "com.novocode" % "junit-interface" % "0.10" % "test"
+    )
+  )
---
+++ samples/composed/client/petstore/java/feign/src/test/java/io/swagger/client/api/PetApiTest.java
@@ -0,0 +1,203 @@
+package io.swagger.client.api;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.model.ModelApiResponse;
+import io.swagger.client.model.Pet;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * API tests for PetApi
+ */
+public class PetApiTest {
+
+    private PetApi api;
+
+    @Before
+    public void setup() {
+        api = new ApiClient().buildClient(PetApi.class);
+    }
+
+    /**
+     * Add a new pet to the store
+     *
+     * 
+     */
+    @Test
+    public void addPetTest() {
+        Pet body = null;
+        // api.addPet(body);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Deletes a pet
+     *
+     * 
+     */
+    @Test
+    public void deletePetTest() {
+        Long petId = null;
+        String apiKey = null;
+        // api.deletePet(petId, apiKey);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Find pet by ID
+     *
+     * schedule pet feeding
+     */
+    @Test
+    public void feedPetTest() {
+        Pet body = null;
+        String token = null;
+        String petType = null;
+        String status = null;
+        Long petId = null;
+        // api.feedPet(body, token, petType, status, petId);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Find pet by ID
+     *
+     * schedule pet feeding
+     *
+     * This tests the overload of the method that uses a Map for query parameters instead of
+     * listing them out individually.
+     */
+    @Test
+    public void feedPetTestQueryMap() {
+        Pet body = null;
+        String token = null;
+        Long petId = null;
+        PetApi.FeedPetQueryParams queryParams = new PetApi.FeedPetQueryParams()
+            .petType(null)
+            .status(null);
+        // api.feedPet(body, token, petId, queryParams);
+
+    // TODO: test validations
+    }
+    /**
+     * Finds Pets by status
+     *
+     * Multiple status values can be provided with comma separated strings
+     */
+    @Test
+    public void findPetsByStatusTest() {
+        List<String> status = null;
+        // List<Pet> response = api.findPetsByStatus(status);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Finds Pets by status
+     *
+     * Multiple status values can be provided with comma separated strings
+     *
+     * This tests the overload of the method that uses a Map for query parameters instead of
+     * listing them out individually.
+     */
+    @Test
+    public void findPetsByStatusTestQueryMap() {
+        PetApi.FindPetsByStatusQueryParams queryParams = new PetApi.FindPetsByStatusQueryParams()
+            .status(null);
+        // List<Pet> response = api.findPetsByStatus(queryParams);
+
+    // TODO: test validations
+    }
+    /**
+     * Finds Pets by tags
+     *
+     * Muliple tags can be provided with comma separated strings. Use\\ \\ tag1, tag2, tag3 for testing.
+     */
+    @Test
+    public void findPetsByTagsTest() {
+        List<String> tags = null;
+        // List<Pet> response = api.findPetsByTags(tags);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Finds Pets by tags
+     *
+     * Muliple tags can be provided with comma separated strings. Use\\ \\ tag1, tag2, tag3 for testing.
+     *
+     * This tests the overload of the method that uses a Map for query parameters instead of
+     * listing them out individually.
+     */
+    @Test
+    public void findPetsByTagsTestQueryMap() {
+        PetApi.FindPetsByTagsQueryParams queryParams = new PetApi.FindPetsByTagsQueryParams()
+            .tags(null);
+        // List<Pet> response = api.findPetsByTags(queryParams);
+
+    // TODO: test validations
+    }
+    /**
+     * Find pet by ID
+     *
+     * Returns a single pet
+     */
+    @Test
+    public void getPetByIdTest() {
+        Long petId = null;
+        // Pet response = api.getPetById(petId);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Update an existing pet
+     *
+     * 
+     */
+    @Test
+    public void updatePetTest() {
+        Pet body = null;
+        // api.updatePet(body);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Updates a pet in the store with form data
+     *
+     * 
+     */
+    @Test
+    public void updatePetWithFormTest() {
+        Long petId = null;
+        String name = null;
+        String status = null;
+        // api.updatePetWithForm(petId, name, status);
+
+        // TODO: test validations
+    }
+
+    /**
+     * uploads an image
+     *
+     * 
+     */
+    @Test
+    public void uploadFileTest() {
+        Long petId = null;
+        Object body = null;
+        // ModelApiResponse response = api.uploadFile(petId, body);
+
+        // TODO: test validations
+    }
+
+}
