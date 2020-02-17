bb554b37a1e22fd17a6bb02194667691840fa168
HugoMario <hugo.mario.mercado@gmail.com>
2020-02-16 23:02:48 -0500

    fixed config files on scripts

--- bin/composed/swift3-petstore-objcCompatible.sh
+++ bin/composed/swift3-petstore-objcCompatible.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
-ags="generate -v -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-composed-schemas.yaml -l swift3 -c ../bin/swift3-petstore-objcCompatible.json -o samples/composed/client/petstore/swift3/objcCompatible $@"
+ags="generate -v -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-composed-schemas.yaml -l swift3 -c bin/swift3-petstore-objcCompatible.json -o samples/composed/client/petstore/swift3/objcCompatible $@"
 
 java $JAVA_OPTS -jar $executable $ags
