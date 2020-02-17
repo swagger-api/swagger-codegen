2e865f248331c0508d77d0347327ac4c46b59805
HugoMario <hugo.mario.mercado@gmail.com>
2020-02-16 23:40:02 -0500

    added jaxrs-cxf-non-spring sample

---
+++ samples/composed/server/petstore/jaxrs-cxf-non-spring-app/src/gen/java/io/swagger/api/UserApi.java
@@ -0,0 +1,136 @@
+package io.swagger.api;
+
+import io.swagger.model.User;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import org.apache.cxf.jaxrs.ext.multipart.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+/**
+ * Swagger Petstore
+ *
+ * <p>This is a sample Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/). 
+ *
+ */
+@Path("/")
+public interface UserApi  {
+
+    /**
+     * Create user
+     *
+     * This can only be done by the logged in user.
+     *
+     */
+    @POST
+    @Path("/user")
+    @Consumes({ "application/json" })
+    @Operation(summary = "Create user", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation") })
+    public void createUser(@Valid User body);
+
+    /**
+     * Creates list of users with given input array
+     *
+     */
+    @POST
+    @Path("/user/createWithArray")
+    @Consumes({ "application/json" })
+    @Operation(summary = "Creates list of users with given input array", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation") })
+    public void createUsersWithArrayInput(@Valid List<User> body);
+
+    /**
+     * Creates list of users with given input array
+     *
+     */
+    @POST
+    @Path("/user/createWithList")
+    @Consumes({ "application/json" })
+    @Operation(summary = "Creates list of users with given input array", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation") })
+    public void createUsersWithListInput(@Valid List<User> body);
+
+    /**
+     * Delete user
+     *
+     * This can only be done by the logged in user.
+     *
+     */
+    @DELETE
+    @Path("/user/{username}")
+    @Operation(summary = "Delete user", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "400", description = "Invalid username supplied"),
+        @ApiResponse(responseCode = "404", description = "User not found") })
+    public void deleteUser(@PathParam("username") String username);
+
+    /**
+     * Get user by user name
+     *
+     */
+    @GET
+    @Path("/user/{username}")
+    @Produces({ "application/json", "application/xml" })
+    @Operation(summary = "Get user by user name", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = User.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid username supplied"),
+        @ApiResponse(responseCode = "404", description = "User not found") })
+    public User getUserByName(@PathParam("username") String username);
+
+    /**
+     * Logs user into the system
+     *
+     */
+    @GET
+    @Path("/user/login")
+    @Produces({ "application/json", "application/xml" })
+    @Operation(summary = "Logs user into the system", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid username/password supplied") })
+    public String loginUser(@QueryParam("username") @NotNull String username, @QueryParam("password") @NotNull String password);
+
+    /**
+     * Logs out current logged in user session
+     *
+     */
+    @GET
+    @Path("/user/logout")
+    @Operation(summary = "Logs out current logged in user session", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation") })
+    public void logoutUser();
+
+    /**
+     * Updated user
+     *
+     * This can only be done by the logged in user.
+     *
+     */
+    @PUT
+    @Path("/user/{username}")
+    @Consumes({ "application/json" })
+    @Operation(summary = "Updated user", tags={ "user" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "400", description = "Invalid user supplied"),
+        @ApiResponse(responseCode = "404", description = "User not found") })
+    public void userUsernamePut(@Valid User body, @PathParam("username") String username);
+}
