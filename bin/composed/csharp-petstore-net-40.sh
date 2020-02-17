90ea6992629b9164a90113c1ed44074d67675f17
HugoMario <hugo.mario.mercado@gmail.com>
2020-02-16 23:02:48 -0500

    fixed config files on scripts

--- bin/composed/csharp-petstore-net-40.sh
+++ bin/composed/csharp-petstore-net-40.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
-ags="generate $@ -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-composed-schemas.yaml -l csharp -o samples/composed/client/petstore/csharp/SwaggerClientNet40 --additional-properties packageGuid={321C8C3F-0156-40C1-AE42-D59761FB9B6C} -c ../bin/csharp-petstore-net-40.json"
+ags="generate $@ -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-composed-schemas.yaml -l csharp -o samples/composed/client/petstore/csharp/SwaggerClientNet40 --additional-properties packageGuid={321C8C3F-0156-40C1-AE42-D59761FB9B6C} -c bin/csharp-petstore-net-40.json"
 
 java $JAVA_OPTS -jar $executable $ags
--- bin/composed/swift3-petstore-promisekit.sh
+++ bin/composed/swift3-petstore-promisekit.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
-ags="generate -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-composed-schemas.yaml -l swift3 -c ../bin/swift3-petstore-promisekit.json -o samples/composed/client/petstore/swift3/promisekit $@"
+ags="generate -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-composed-schemas.yaml -l swift3 -c bin/swift3-petstore-promisekit.json -o samples/composed/client/petstore/swift3/promisekit $@"
 
 java $JAVA_OPTS -jar $executable $ags
