f96c31f9aaaf2bcd0ef53acaa52bd052cdd5907c
HugoMario <hugo.mario.mercado@gmail.com>
2020-02-16 23:34:03 -0500

    added java retrofit2rx2 sample

---
+++ samples/composed/client/petstore/java/retrofit2rx2/src/main/java/io/swagger/client/api/DefaultApi.java
@@ -0,0 +1,27 @@
+package io.swagger.client.api;
+
+import io.swagger.client.CollectionFormats.*;
+
+import io.reactivex.Observable;
+import retrofit2.http.*;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface DefaultApi {
+  /**
+   * 
+   * 
+   * @return Call&lt;List&lt;String&gt;&gt;
+   */
+  @GET("test")
+  Observable<List<String>> testMethod();
+    
+
+}
