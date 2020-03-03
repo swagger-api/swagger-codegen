e7f3d45b9e7842b72dea348c2fbc5545178ccf3c
HugoMario <hugo.mario.mercado@gmail.com>
2020-03-03 02:39:54 -0500

    use v3 definition for retrofit2 and jaxrs scripts

--- bin/java-petstore-retrofit2-play25.sh
+++ bin/java-petstore-retrofit2-play25.sh
@@ -26,7 +26,7 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
-ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml -l java -c bin/java-petstore-retrofit2-play25.json -o samples/client/petstore/java/retrofit2-play25 -DhideGenerationTimestamp=true"
+ags="$@ generate -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-fake-endpoints-models-for-testing.yaml -l java -c bin/java-petstore-retrofit2-play25.json -o samples/client/petstore/java/retrofit2-play25 -DhideGenerationTimestamp=true"
 
 echo "Removing files and folders under samples/client/petstore/java/retrofit2-play25/src/main"
 rm -rf samples/client/petstore/java/retrofit2-play25/src/main
