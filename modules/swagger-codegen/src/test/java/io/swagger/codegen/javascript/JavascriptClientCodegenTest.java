9dcec5784e81a58941124e3e591f9d147e1d180a
HugoMario <hugo.mario.mercado@gmail.com>
2020-03-21 07:25:19 -0500

    fixed recursive model break issue for javascript generator

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/javascript/JavascriptClientCodegenTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/javascript/JavascriptClientCodegenTest.java
@@ -1,10 +1,16 @@
 package io.swagger.codegen.javascript;
 
+import io.swagger.codegen.ClientOptInput;
+import io.swagger.codegen.DefaultGenerator;
+import io.swagger.codegen.config.CodegenConfigurator;
+import org.junit.rules.TemporaryFolder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.swagger.codegen.CodegenConstants;
 import io.swagger.codegen.languages.JavascriptClientCodegen;
+
+import java.io.File;
 
 public class JavascriptClientCodegenTest {
 
@@ -41,6 +47,44 @@ public class JavascriptClientCodegenTest {
 
         Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.FALSE);
         Assert.assertEquals(codegen.isHideGenerationTimestamp(), false);
+    }
+
+    @Test
+    public void testRecursiveModel() {
+        //final TemporaryFolder folder = new TemporaryFolder();
+
+        //folder.create();
+        //final File output = folder.getRoot();
+
+        final CodegenConfigurator configurator = new CodegenConfigurator()
+                .setLang("javascript")
+                .setInputSpec("src/test/resources/2_0/recursive_model.yaml")
+                .setOutputDir("/Users/hugomercado/Dropbox/Documents/swagger-codegen/output");
+
+        final ClientOptInput clientOptInput = configurator.toClientOptInput();
+        new DefaultGenerator().opts(clientOptInput).generate();
+
+
+        //folder.delete();
+
+//        final TemporaryFolder folder = new TemporaryFolder();
+//
+//        folder.create();
+//        final File output = folder.getRoot();
+//
+//        final CodegenConfigurator configurator = new CodegenConfigurator()
+//                .setLang("javascript")
+//                .setInputSpec("src/test/resources/2_0/recursive_model.yaml")
+//                .setOutputDir(output.getAbsolutePath());
+//
+//        final ClientOptInput clientOptInput = configurator.toClientOptInput();
+//        new DefaultGenerator().opts(clientOptInput).generate();
+//
+//        final File orderFile = new File(output, "src/api/TestClassApi.js");
+//        Assert.assertTrue(orderFile.exists());
+//
+//        folder.delete();
+
     }
 
 }
