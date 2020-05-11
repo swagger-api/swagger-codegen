de76ff628f378957ab2356157bcf90ce6b722ec4
awqat <awqat.app@gmail.com>
2020-05-11 17:11:51 +0200

    #10107 Combine custom templates and embedded templates

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/v3/templates/HandlebarTemplateEngine.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/v3/templates/HandlebarTemplateEngine.java
@@ -1,13 +1,16 @@
 package io.swagger.codegen.v3.templates;
 
 import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.CompositeTemplateLoader;
 import com.github.jknack.handlebars.io.FileTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 import io.swagger.codegen.v3.CodegenConfig;
 import io.swagger.codegen.v3.CodegenConstants;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -30,21 +33,50 @@ public class HandlebarTemplateEngine implements TemplateEngine {
         return CodegenConstants.HANDLEBARS_TEMPLATE_ENGINE;
     }
 
-    private com.github.jknack.handlebars.Template getHandlebars(String templateFile) throws IOException {
+    /**
+     *     
+     * @param originalTemplateFile the template file 
+     * 
+     * @return return the template 
+     * 
+     * @throws IOException
+     */
+    private Template getHandlebars(String originalTemplateFile) throws IOException
+    {
+
+        String templateFile = originalTemplateFile;
         templateFile = templateFile.replace(".mustache", StringUtils.EMPTY).replace("\\", "/");
+
+        final TemplateLoader embeddedTemplateLoader = new ClassPathTemplateLoader("/" + config.embeddedTemplateDir(), ".mustache");
+        final TemplateLoader templateLoader;
+        
+        // Get the custom template dir
         String templateDir = config.templateDir().replace(".mustache", StringUtils.EMPTY).replace("\\", "/");
-        if (templateFile.startsWith(templateDir)) {
-            templateFile = StringUtils.replaceOnce(templateFile, templateDir, StringUtils.EMPTY);
+        if (config.additionalProperties().get(CodegenConstants.TEMPLATE_DIR) != null && (new File(originalTemplateFile).exists() || new File(templateDir+originalTemplateFile).exists() ))
+        {
+            if (templateFile.startsWith(templateDir))
+            {
+                templateFile = StringUtils.replaceOnce(templateFile, templateDir, StringUtils.EMPTY);
+            }
+            // Using CompositeTemplateLoader to manage partial templates
+            templateLoader = new CompositeTemplateLoader(new FileTemplateLoader(templateDir, ".mustache"),
+                embeddedTemplateLoader);
         }
-        TemplateLoader templateLoader = null;
-        if (config.additionalProperties().get(CodegenConstants.TEMPLATE_DIR) != null) {
-            templateLoader = new FileTemplateLoader(templateDir, ".mustache");
-        } else {
-            templateLoader = new ClassPathTemplateLoader("/" + templateDir, ".mustache");
+        // Get the embedded template dir
+        else
+        {
+            if (templateFile.startsWith(config.embeddedTemplateDir()))
+            {
+                templateFile = StringUtils.replaceOnce(templateFile, config.embeddedTemplateDir(), StringUtils.EMPTY);
+            }
+
+            templateLoader = embeddedTemplateLoader;
         }
+
         final Handlebars handlebars = new Handlebars(templateLoader);
         handlebars.prettyPrint(true);
         config.addHandlebarHelpers(handlebars);
         return handlebars.compile(templateFile);
     }
+
 }
