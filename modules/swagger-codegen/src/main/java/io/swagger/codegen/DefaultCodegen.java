d29aee62f4151cb79b00bdb28708d65cec46c30b
Dmitriy Kondrashkin <dkondrashkin@gmail.com>
2020-06-05 01:03:50 +0300

    Fix for string list parameter code generation

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java
@@ -2726,7 +2726,7 @@ public class DefaultCodegen {
                     innerCp = innerCp.items;
                 }
 
-                p.items = cp;
+                p.items = cp.items;
                 p.dataType = cp.datatype;
                 p.baseType = cp.complexType;
                 p.isPrimitiveType = cp.isPrimitiveType;
