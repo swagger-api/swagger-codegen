24e4e0aa03cc5f66eae483a6de17ef0608890081
Krishna Sankar <krishna.sankar@capillarytech.com>
2020-08-13 16:23:36 +0530

    file service script url updated

--- swagger_sdk_gen_fileService.sh
+++ swagger_sdk_gen_fileService.sh
@@ -3,15 +3,15 @@ if $Build_Codegen ; then
 fi
 if [ "$Branch" = "master" ]
   then
-    url="http://fileservice.capillary.in/tl-docs-test/v2/api-docs"
-    version="https://fileservice.capillary.in/v1/meta/version"
+    url="https://crm-nightly-new.cc.capillarytech.com/api_gateway/file-service/v2/api-docs"
+    version="https://crm-nightly-new.cc.capillarytech.com/api_gateway/file-service/v1/meta/version"
 elif [ "$Branch" = "production" ]
   then
     url="http://fileservice.capillary.in/tl-docs-test/v2/api-docs"
     version="https://fileservice.capillary.in/v1/meta/version"
 else " No Branch is selected"
 fi
-curl -k $version -o config.json
+curl -k $version -o config.json -H"Authorization: Basic a3Jpc2huYS50aWxsMDE6MjAyY2I5NjJhYzU5MDc1Yjk2NGIwNzE1MmQyMzRiNzA="
 echo "GENERATING SDK"
 if [ "$Client" = "java" ]
 then 
@@ -22,7 +22,7 @@ then
   -DdateLibrary=java8 \
   -o fileservice/java_client/java \
   -c config.json
-  tar cvzf fileservice/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
+  tar cvzf fileservice/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./filservice/java_client/java/ .
   mvn3 clean deploy -f fileservice/java_client/java/pom.xml
   fpm -f -s "dir" -t "deb" -a "all" -n "swagger_sdk_gen_fileService" -v $BUILD_NUMBER -C ./fileservice/java_client --deb-no-default-config-files  java="/usr/share/java/capillary-libs/swagger_sdk_gen_fileService"
 else " no client is selected"
