c0291bfd54f4f1f2edca07c0f21e11494835b07b
Krishna Sankar <krishna.sankar@capillarytech.com>
2020-08-13 17:52:11 +0530

    file sevice correction

--- swagger_sdk_gen_fileService.sh
+++ swagger_sdk_gen_fileService.sh
@@ -12,14 +12,14 @@ elif [ "$Branch" = "production" ]
 else " No Branch is selected"
 fi
 ver=$(curl -H"Authorization: Basic a3Jpc2huYS50aWxsMDE6MjAyY2I5NjJhYzU5MDc1Yjk2NGIwNzE1MmQyMzRiNzA=" -k $version)
-echo {"artifactVersion":$ver} > config.json
+echo {'"artifactVersion"':\"$ver\"} > config.json
 echo "GENERATING SDK"
+curl -H"Authorization:Basic a3Jpc2huYS50aWxsMDE6MjAyY2I5NjJhYzU5MDc1Yjk2NGIwNzE1MmQyMzRiNzA=" -o spec.json "https://crm-nightly-new.cc.capillarytech.com/api_gateway/file-service/v2/api-docs"
 if [ "$Client" = "java" ]
-then 
+then
   rm -rf fileservice/java_client/java
   java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-  -a "Authorization%3ABasic%20a3Jpc2huYS50aWxsMDE6MjAyY2I5NjJhYzU5MDc1Yjk2NGIwNzE1MmQyMzRiNzA%3D" \
-  -i $url  \
+  -i spec.json  \
   -l java \
   -DdateLibrary=java8 \
   -o fileservice/java_client/java \
