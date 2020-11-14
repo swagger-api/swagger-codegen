52e95f37df49f52a488cd96f410827950d5f43fe
Hugo Mercado <hugo.mario.mercado@gmail.com>
2020-11-14 04:24:13 -0500

    added list as reserved word for java generator

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AbstractJavaCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/AbstractJavaCodegen.java
@@ -122,7 +122,7 @@ public abstract class AbstractJavaCodegen extends DefaultCodegen implements Code
                 "this", "break", "double", "implements", "protected", "throw", "byte", "else",
                 "import", "public", "throws", "case", "enum", "instanceof", "return", "transient",
                 "catch", "extends", "int", "short", "try", "char", "final", "interface", "static",
-                "void", "class", "finally", "long", "strictfp", "volatile", "const", "float",
+                "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "list",
                 "native", "super", "while", "null")
         );
 
