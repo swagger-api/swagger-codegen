cef1904dd087426623dba25c69527ccec97f57db
Tim Smith <tim.j.smith@genesys.com>
2020-11-24 09:23:17 -0700

    DEVENGAGE-287 add initial web messaging SDK config

---
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/WebMessagingJavaClientCodegen.java
@@ -0,0 +1,60 @@
+package io.swagger.codegen.languages;
+
+import io.swagger.codegen.SupportingFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebMessagingJavaClientCodegen extends PureCloudJavaClientCodegen {
+    protected Logger LOGGER = LoggerFactory.getLogger(WebMessagingJavaClientCodegen.class);
+
+    public WebMessagingJavaClientCodegen() {
+        super();
+    }
+
+    @Override
+    public String getName() {
+        return "webmessagingjava";
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+        // Wipe out all the templates so we can start with a clean slate
+        this.apiTemplateFiles.clear();
+        this.apiDocTemplateFiles.clear();
+        this.apiTestTemplateFiles.clear();
+        this.modelTemplateFiles.clear();
+        this.modelDocTemplateFiles.clear();
+        this.modelTestTemplateFiles.clear();
+        this.operationTemplateFiles.clear();
+        this.supportingFiles.clear();
+
+        modelTemplateFiles.put("model.mustache", ".java");
+        modelDocTemplateFiles.put("model_doc.mustache", ".md");
+
+        supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml"));
+        supportingFiles.add(new SupportingFile("gitignore.mustache", "", ".gitignore"));
+        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+//        supportingFiles.add(new SupportingFile("testng.mustache", "", "testng.xml"));
+
+        final String invokerFolder = (sourceFolder + '/' + invokerPackage).replace(".", "/");
+        supportingFiles.add(new SupportingFile("WebMessagingClient.mustache", invokerFolder, "WebMessagingClient.java"));
+        supportingFiles.add(new SupportingFile("WebMessagingException.mustache", invokerFolder, "WebMessagingException.java"));
+        supportingFiles.add(new SupportingFile("GenesysCloudRegionHosts.mustache", invokerFolder, "GenesysCloudRegionHosts.java"));
+        supportingFiles.add(new SupportingFile("ApiDateFormat.mustache", invokerFolder, "ApiDateFormat.java"));
+        supportingFiles.add(new SupportingFile("LocalDateDeserializer.mustache", invokerFolder, "LocalDateDeserializer.java"));
+    }
+
+    @Override
+    public String getterAndSetterCapitalize(String name) {
+        String sanitizedName = name;
+
+        // Special handling to avoid a method conflict named "getClass"
+        if ("class".equalsIgnoreCase(name)) {
+            sanitizedName = "classProperty";
+        }
+
+        return super.getterAndSetterCapitalize(sanitizedName);
+    }
+}
