9c97105ad234a6a5cd36e076ffdfaae7fc399030
Krishna Sankar <krishna.sankar@capillarytech.com>
2021-05-05 11:45:59 +0530

    date format in v3

--- swagger_sdk_gen_v3.sh
+++ swagger_sdk_gen_v3.sh
@@ -17,10 +17,9 @@ echo "GENERATING SDK"
 if [ "$Client" = "java" ]
 then 
   rm -rf intouch_api/java_client/java
-  java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+  java -Dio.swagger.parser.util.RemoteUrl.trustAll=true -DdateLibrary=java8 -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i $url  \
   -l java \
-  -Dio.swagger.parser.util.RemoteUrl.trustAll=true -DdateLibrary=java8 \
   -o intouch_api/java_client/java \
   -c config.json
   tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
