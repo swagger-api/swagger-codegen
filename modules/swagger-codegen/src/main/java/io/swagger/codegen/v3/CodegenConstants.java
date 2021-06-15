9c5931aa4a5abcccafb195cef2e699a7217e471a
Hugo Mercado <hugo.mario.mercado@gmail.com>
2021-06-15 11:12:40 -0500

    added kebab case on model property  naming type enumeration

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/v3/CodegenConstants.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/v3/CodegenConstants.java
@@ -161,8 +161,8 @@ public class CodegenConstants {
     public static final String DOTNET_FRAMEWORK = "targetFramework";
     public static final String DOTNET_FRAMEWORK_DESC = "The target .NET framework version.";
 
-    public static enum MODEL_PROPERTY_NAMING_TYPE {camelCase, PascalCase, snake_case, original}
-    public static enum ENUM_PROPERTY_NAMING_TYPE {camelCase, PascalCase, snake_case, original, UPPERCASE}
+    public static enum MODEL_PROPERTY_NAMING_TYPE {camelCase, PascalCase, snake_case, kebab_case, original}
+    public static enum ENUM_PROPERTY_NAMING_TYPE {camelCase, PascalCase, snake_case, kebab_case, original, UPPERCASE}
 
     public static final String ENUM_PROPERTY_NAMING = "enumPropertyNaming";
     public static final String ENUM_PROPERTY_NAMING_DESC = "Naming convention for enum properties: 'camelCase', 'PascalCase', 'snake_case', 'UPPERCASE', and 'original'";
