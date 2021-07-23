c97a96c9d39e3466a9152a948ac727412f904d41
Hugo Mercado <hugo.mario.mercado@gmail.com>
2021-07-22 22:44:41 -0500

    added samples for typescript angular versions 7 to 11

---
+++ samples/client/petstore/typescript-angular-v7/api/api.ts
@@ -0,0 +1,17 @@
+export * from './animal.service';
+import { AnimalService } from './animal.service';
+export * from './anotherFake.service';
+import { AnotherFakeService } from './anotherFake.service';
+export * from './dog.service';
+import { DogService } from './dog.service';
+export * from './fake.service';
+import { FakeService } from './fake.service';
+export * from './fakeClassnameTags123.service';
+import { FakeClassnameTags123Service } from './fakeClassnameTags123.service';
+export * from './pet.service';
+import { PetService } from './pet.service';
+export * from './store.service';
+import { StoreService } from './store.service';
+export * from './user.service';
+import { UserService } from './user.service';
+export const APIS = [AnimalService, AnotherFakeService, DogService, FakeService, FakeClassnameTags123Service, PetService, StoreService, UserService];
