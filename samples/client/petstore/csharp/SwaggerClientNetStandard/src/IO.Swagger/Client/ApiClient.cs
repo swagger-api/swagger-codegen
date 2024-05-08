4df54e2157f7cfaa84d0cc1c6283e40d7a9c2a73
MicRyc <michal.rychel98@gmail.com>
2024-05-08 15:06:41 +0200

    Update swagger-generators version to 1.0.49-SNAP and generated Csharp samples

--- samples/client/petstore/csharp/SwaggerClientNetStandard/src/IO.Swagger/Client/ApiClient.cs
+++ samples/client/petstore/csharp/SwaggerClientNetStandard/src/IO.Swagger/Client/ApiClient.cs
@@ -25,7 +25,7 @@ namespace IO.Swagger.Client
     /// <summary>
     /// API client is mainly responsible for making the HTTP call to the API backend.
     /// </summary>
-        public partial class ApiClient
+    public partial class ApiClient
     {
         private JsonSerializerSettings serializerSettings = new JsonSerializerSettings
         {
