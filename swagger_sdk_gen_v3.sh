e4bda4fb12d1b511d1f5a5d966ab41ed83cced9d
Devansh Singh <devansh.singh@capillarytech.com>
2024-11-29 02:39:29 +0530

    upgrade to swagger 3 for sdk

--- swagger_sdk_gen_v3.sh
+++ swagger_sdk_gen_v3.sh
@@ -18,9 +18,8 @@ if [ "$Client" = "java" ]
 then
   rm -rf intouch_api/java_client/java
     java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-    -i $url  \
+    -i swagger.json  \
     -l java \
-    -DdateLibrary=java8 \
     -o intouch_api/java_client/java \
     -c config.json
     tar cvzf intouch_api/java_client/java_swagger_sdk_$BUILD_NUMBER.tar.gz -C ./intouch_api/java_client/java/ .
