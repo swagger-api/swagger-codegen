d1bf37e458c3e8433229e4656b73e3af281ef9e0
Devansh Singh <devansh.singh@capillarytech.com>
2024-11-29 12:40:09 +0530

    --

--- swagger_sdk_gen_v3.sh
+++ swagger_sdk_gen_v3.sh
@@ -22,7 +22,7 @@ then
     # Step 2: Generate the SDK using OpenAPI Generator (via Docker)
     echo "Generating SDK using OpenAPI Generator..."
     docker run --rm -v $(pwd):/local openapitools/openapi-generator-cli generate \
-        -i swagger.json \
+        -i $url \
         -g java \
         -p dateLibrary=java8 \
         -o intouch_api/java_client/java \
